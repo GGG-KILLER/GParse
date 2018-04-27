132f21120c5174dbc3af33b63cabbd61d801fc66
GGG KILLER <gggkiller2@gmail.com>
2018-04-27 11:28:36 -0300

    CLI project

---
+++ GParse.CLI/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using GParse.Verbose;
+using GParse.Verbose.Matchers;
+
+namespace GParse.CLI
+{
+    internal class Program
+    {
+        private static void Main ( )
+        {
+            var lang = new Language ( "Calculator" );
+            BaseMatcher @int = ( Match.ByFilter ( Char.IsDigit ) * -1 ).As ( "integer" );
+            BaseMatcher num = ( @int
+                + ~( Match.Char ( '.' ) + @int )
+                + ~( Match.Chars ( 'E', 'e' ) + Match.Chars ( '+', '-' ).Optional ( ) + @int ) ).As ( "number" );
+        }
+    }
+}
