6b6797ff5cabd780cf1959094f48c34543834580
GGG KILLER <gggkiller2@gmail.com>
2018-05-10 22:44:33 -0300

    Store original message

--- GParse.Common/Errors/ParseException.cs
+++ GParse.Common/Errors/ParseException.cs
@@ -5,10 +5,12 @@ namespace GParse.Common.Errors
     public class ParseException : Exception
     {
         public SourceLocation Location;
+        public String OriginalMessage;
 
         public ParseException ( SourceLocation sourceLocation, String message ) : base ( $"{sourceLocation.Line}:{sourceLocation.Column}  {message}" )
         {
             this.Location = sourceLocation;
+            this.OriginalMessage = message;
         }
     }
 }
