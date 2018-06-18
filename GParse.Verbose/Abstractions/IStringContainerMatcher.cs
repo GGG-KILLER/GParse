5add98fa82fc8e24e3726920bb353ce347ca73db
GGG KILLER <gggkiller2@gmail.com>
2018-06-18 19:06:40 -0300

    Add interface to group CharMatcher and StringMatcher

---
+++ GParse.Verbose/Abstractions/IStringContainerMatcher.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GParse.Verbose.Abstractions
+{
+    interface IStringContainerMatcher
+    {
+        String StringFilter { get; }
+    }
+}
