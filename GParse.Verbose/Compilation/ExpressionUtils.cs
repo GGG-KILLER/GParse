5ef6c9c120298ad48b6b15ec7ddb469264d43a8e
GGG KILLER <gggkiller2@gmail.com>
2018-06-18 19:04:14 -0300

    Expression compilation (WIP)

---
+++ GParse.Verbose/Compilation/ExpressionUtils.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+using GParse.Common.IO;
+
+namespace GParse.Verbose.Compilation
+{
+    public static class ExpressionUtils
+    {
+        #region GetMethodCallExpression
+
+        public static Expression GetMethodCallExpression<Class> ( Expression inst, String name, params Expression[] args )
+        {
+            return Expression.Call ( inst, typeof ( Class ).GetMethod ( name ), args );
+        }
+
+        public static Expression GetMethodCallExpression<Class, A1> ( Expression inst, String name, params Expression[] args )
+        {
+            return Expression.Call ( inst, typeof ( Class ).GetMethod ( name, new[] {
+                typeof ( A1 )
+            } ), args );
+        }
+
+        public static Expression GetMethodCallExpression<Class, A1, A2> ( Expression inst, String name, params Expression[] args )
+        {
+            return Expression.Call ( inst, typeof ( Class ).GetMethod ( name, new[] {
+                typeof ( A1 ),
+                typeof ( A2 )
+            } ), args );
+        }
+
+        public static Expression GetMethodCallExpression<Class, A1, A2, A3> ( Expression inst, String name, params Expression[] args )
+        {
+            return Expression.Call ( inst, typeof ( Class ).GetMethod ( name, new[] {
+                typeof ( A1 ),
+                typeof ( A2 ),
+                typeof ( A3 )
+            } ), args );
+        }
+
+        public static Expression GetMethodCallExpression<Class, A1, A2, A3, A4> ( Expression inst, String name, params Expression[] args )
+        {
+            return Expression.Call ( inst, typeof ( Class ).GetMethod ( name, new[] {
+                typeof ( A1 ),
+                typeof ( A2 ),
+                typeof ( A3 ),
+                typeof ( A4 )
+            } ), args );
+        }
+
+        public static Expression GetMethodCallExpression<Class, A1, A2, A3, A4, A5> ( Expression inst, String name, params Expression[] args )
+        {
+            return Expression.Call ( inst, typeof ( Class ).GetMethod ( name, new[] {
+                typeof ( A1 ),
+                typeof ( A2 ),
+                typeof ( A3 ),
+                typeof ( A4 ),
+                typeof ( A5 )
+            } ), args );
+        }
+
+        public static Expression GetMethodCallExpression<Class, A1, A2, A3, A4, A5, A6> ( Expression inst, String name, params Expression[] args )
+        {
+            return Expression.Call ( inst, typeof ( Class ).GetMethod ( name, new[] {
+                typeof ( A1 ),
+                typeof ( A2 ),
+                typeof ( A3 ),
+                typeof ( A4 ),
+                typeof ( A5 ),
+                typeof ( A6 )
+            } ), args );
+        }
+
+        #endregion GetMethodCallExpression
+
+        public static Expression GetLambdaExpressionCallExpression ( LambdaExpression lambda, params Expression[] args )
+        {
+            return Expression.Call ( lambda.Compile ( ).Method, args );
+        }
+    }
+}
---
+++ GParse.Verbose/Compilation/MatchCompiler.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using GParse.Common.IO;
+using GParse.Verbose.Matchers;
+
+namespace GParse.Verbose
+{
+    public delegate String[] CompiledRuleDelegate ( SourceCodeReader reader );
+
+#pragma warning disable CC0091
+#pragma warning disable CC0057
+
+    public class MatchCompiler : MatcherTreeVisitor<Expression>
+    {
+        private readonly Dictionary<String, CompiledRuleDelegate> CompilationCache = new Dictionary<String, CompiledRuleDelegate> ( );
+        private readonly List<Expression> MethodBody = new List<Expression> ( );
+        private readonly LabelTarget ReturnLabel = Expression.Label ( typeof ( String[] ) );
+        private readonly ParameterExpression SourceReader = Expression.Parameter ( typeof ( SourceCodeReader ), "reader" );
+        private readonly ParameterExpression ReturnList = Expression.Variable ( typeof ( List<String> ), "return" );
+
+        public override Expression Visit ( AllMatcher allMatcher )
+        {
+            throw new NotImplementedException ( );
+        }
+
+        public override Expression Visit ( AnyMatcher anyMatcher )
+        {
+            throw new NotImplementedException ( );
+        }
+
+        public override Expression Visit ( CharMatcher charMatcher )
+        {
+            throw new NotImplementedException ( );
+        }
+
+        public override Expression Visit ( CharRangeMatcher charRangeMatcher )
+        {
+            throw new NotImplementedException ( );
+        }
+
+        public override Expression Visit ( FilterFuncMatcher filterFuncMatcher )
+        {
+            throw new NotImplementedException ( );
+        }
+
+        public override Expression Visit ( MultiCharMatcher multiCharMatcher )
+        {
+            throw new NotImplementedException ( );
+        }
+
+        public override Expression Visit ( RulePlaceholder rulePlaceholder )
+        {
+            throw new NotImplementedException ( );
+        }
+
+        public override Expression Visit ( StringMatcher stringMatcher )
+        {
+            throw new NotImplementedException ( );
+        }
+
+        public override Expression Visit ( IgnoreMatcher ignoreMatcher )
+        {
+            throw new NotImplementedException ( );
+        }
+
+        public override Expression Visit ( JoinMatcher joinMatcher )
+        {
+            throw new NotImplementedException ( );
+        }
+
+        public override Expression Visit ( NegatedMatcher negatedMatcher )
+        {
+            throw new NotImplementedException ( );
+        }
+
+        public override Expression Visit ( OptionalMatcher optionalMatcher )
+        {
+            throw new NotImplementedException ( );
+        }
+
+        public override Expression Visit ( RepeatedMatcher repeatedMatcher )
+        {
+            throw new NotImplementedException ( );
+        }
+
+        public override Expression Visit ( RuleWrapper ruleWrapper )
+        {
+            throw new NotImplementedException ( );
+        }
+
+        public override Expression Visit ( MarkerMatcher markerMatcher )
+        {
+            throw new NotImplementedException ( );
+        }
+
+        public override Expression Visit ( EOFMatcher eofMatcher )
+        {
+            throw new NotImplementedException ( );
+        }
+    }
+}
