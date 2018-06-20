bb358896b3d5b332d80b5085aa722049d5097f48
GGG KILLER <gggkiller2@gmail.com>
2018-06-20 03:53:52 -0300

    Added a very generic visitor base

--- GParse.Verbose/GlobalSuppressions.cs
+++ GParse.Verbose/GlobalSuppressions.cs
@@ -11,11 +11,10 @@
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage ( "Style", "CC0013:Use ternary operator", Justification = "<Pending>", Scope = "member", Target = "~M:GParse.Verbose.Parsing.MatchExpressionParser.ParsePrefixedExpression(System.Boolean)~GParse.Verbose.Matchers.BaseMatcher" )]
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage ( "Style", "CC0013:Use ternary operator", Justification = "<Pending>", Scope = "member", Target = "~M:GParse.Verbose.Parsing.MatchExpressionParser.ParseSuffixedExpression(GParse.Verbose.Matchers.BaseMatcher)~GParse.Verbose.Matchers.BaseMatcher" )]
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage ( "Design", "CC0091:Use static method", Justification = "<Pending>", Scope = "member", Target = "~M:GParse.Verbose.VerboseParser.ParseMatcher(System.Func{System.Char,System.Boolean})~GParse.Verbose.Matchers.BaseMatcher" )]// This file is used by Code Analysis to maintain SuppressMessage
-
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage ( "Style", "CC0013:Use ternary operator", Justification = "<Pending>", Scope = "member", Target = "~M:GParse.Verbose.VerboseParser.InfixOperator(System.String,GParse.Verbose.OperatorAssociativity,System.String,GParse.Verbose.Matchers.BaseMatcher,GParse.Verbose.Matchers.BaseMatcher,System.Boolean)~GParse.Verbose.Matchers.BaseMatcher" )]
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage ( "Style", "CC0013:Use ternary operator", Justification = "<Pending>", Scope = "member", Target = "~M:GParse.Verbose.Visitors.ExpressionReconstructor.Visit(GParse.Verbose.Matchers.RepeatedMatcher)~System.String" )]
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage ( "Style", "IDE0028:Simplify collection initialization", Justification = "<Pending>", Scope = "member", Target = "~M:GParse.Verbose.Visitors.StepByStep.StepByStepRecorder.Execute(System.String)~GParse.Verbose.Visitors.StepByStep.Step[]" )]// attributes that are applied to this project. Project-level
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage ( "Design", "CC0031:Check for null before calling a delegate", Justification = "<Pending>", Scope = "member", Target = "~M:GParse.Verbose.Visitors.CompiledBaseVisitor`2.Visit(`0)~`1" )]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage ( "Design", "CC0091:Use static method", Justification = "<Pending>", Scope = "member", Target = "~M:GParse.Verbose.Visitors.CompiledBaseVisitor`2.Visit(`0)~`1" )]
 // suppressions either have no target or are given a specific
 // target and scoped to a namespace, type, member, etc.
-
-
