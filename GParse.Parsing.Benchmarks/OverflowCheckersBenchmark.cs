f493022a4c7f811b20025d3be106b75e9f9b866b
GGG KILLER <gggkiller2@gmail.com>
2018-07-02 05:00:33 -0300

    Benchmarking a few things

---
+++ GParse.Parsing.Benchmarks/OverflowCheckersBenchmark.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Attributes.Columns;
+using BenchmarkDotNet.Attributes.Exporters;
+using BenchmarkDotNet.Attributes.Jobs;
+
+namespace GParse.Parsing.Benchmarks
+{
+    [ClrJob, CoreJob ( )]
+    [RPlotExporter, RankColumn, HtmlExporter, MarkdownExporter]
+    public class OverflowCheckersBenchmark
+    {
+        [Params ( 43054U, 1051313684U )]
+        public UInt32 LeftHandSide;
+
+        [Params ( 11821U, 414902732U )]
+        public UInt32 RightHandSide;
+
+        [Benchmark]
+        public Boolean WillOverflowUnchecked ( )
+        {
+            UInt32 res = unchecked(this.LeftHandSide * this.RightHandSide);
+            return res % this.LeftHandSide != 0 || res % this.RightHandSide != 0;
+        }
+
+        [Benchmark]
+        public Boolean WillOverflowChecked ( )
+        {
+            try
+            {
+                UInt32 res = checked(this.LeftHandSide * this.RightHandSide);
+                return true;
+            }
+            catch ( OverflowException )
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ GParse.Parsing.Benchmarks/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Diagnostics;
+using BenchmarkDotNet.Reports;
+using BenchmarkDotNet.Running;
+
+namespace GParse.Parsing.Benchmarks
+{
+    internal class Program
+    {
+        private static void Main ( )
+        {
+            BenchmarkRunner.Run<OverflowCheckersBenchmark> ( );
+            //BenchmarkRunner.Run<TokenManagerBenchmarks> ( );
+        }
+    }
+}
