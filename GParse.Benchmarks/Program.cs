8095139a1da71f8813c90230d2a42220bced7e4f
GGG KILLER <gggkiller2@gmail.com>
2018-09-06 13:09:10 -0300

    Internal benchmarks for development choices

---
+++ GParse.Benchmarks/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Diagnostics;
+using BenchmarkDotNet.Reports;
+using BenchmarkDotNet.Running;
+
+namespace GParse.Benchmarks
+{
+    internal class Program
+    {
+        private static void Main ( String[] args )
+        {
+            BenchmarkSwitcher
+                .FromAssembly ( typeof ( Program ).Assembly )
+                .Run ( args );
+        }
+    }
+}
