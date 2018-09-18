4b9b5acbe8604da181a231c5e5b236eb04b87e03
GGG KILLER <gggkiller2@gmail.com>
2018-09-17 22:00:01 -0300

    Fix minimal expression generator

--- GParse.Fluent/Visitors/MinimalExpression/MinimalExpressionGenerator.cs
+++ GParse.Fluent/Visitors/MinimalExpression/MinimalExpressionGenerator.cs
@@ -1,167 +1,140 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 using GParse.Fluent.Abstractions;
 using GParse.Fluent.Matchers;
 
 namespace GParse.Fluent.Visitors.MinimalExpression
 {
-    public class MinimalExpressionsGenerator : IMatcherTreeVisitor<MinimalExpressionNode>
+    public class MinimalExpressionsGenerator : IMatcherTreeVisitor<IEnumerable<StringBuilder>>
     {
         private readonly FluentParser Parser;
-        private readonly Boolean Negated;
 
-        public MinimalExpressionsGenerator ( FluentParser parser, Boolean negated = false )
+        #region Helper funcs
+
+        private static IEnumerable<StringBuilder> CartesianProduct ( IEnumerable<StringBuilder> lhs, IEnumerable<StringBuilder> rhs )
+        {
+            foreach ( StringBuilder a in lhs )
+                foreach ( StringBuilder b in rhs )
+                    yield return new StringBuilder ( a.ToString ( ) + b );
+        }
+
+        #endregion Helper funcs
+
+        public MinimalExpressionsGenerator ( FluentParser parser )
         {
             this.Parser = parser;
-            this.Negated = negated;
         }
 
-        public MinimalExpressionNode Visit ( SequentialMatcher sequentialMatcher )
+        public String[] Generate ( BaseMatcher matcher )
         {
-            var root = sequentialMatcher.PatternMatchers[0].Accept ( this );
+            return matcher.Accept ( this )
+                .Select ( builder => builder.ToString ( ) )
+                .ToArray ( );
+        }
+
+        #region IMatcherTreeVisitor<IEnumerable<StringBuilder>>
+
+        public IEnumerable<StringBuilder> Visit ( SequentialMatcher sequentialMatcher )
+        {
+            IEnumerable<StringBuilder> last = sequentialMatcher.PatternMatchers[0].Accept ( this );
             for ( var i = 1; i < sequentialMatcher.PatternMatchers.Length; i++ )
-            {
-                var child = sequentialMatcher.PatternMatchers[i].Accept ( this );
-                root.AddChild ( child );
-                root = child;
-            }
-            return root;
+                last = CartesianProduct ( last, sequentialMatcher.PatternMatchers[i].Accept ( this ) );
+            return last;
         }
 
-        public MinimalExpressionNode Visit ( AlternatedMatcher alternatedMatcher )
+        public IEnumerable<StringBuilder> Visit ( AlternatedMatcher alternatedMatcher )
         {
-            var node = MinimalExpressionNode.CreateNew ( );
             for ( var i = 0; i < alternatedMatcher.PatternMatchers.Length; i++ )
-                node.AddChild ( alternatedMatcher.PatternMatchers[i].Accept ( this ) );
-            return node;
+                foreach ( StringBuilder elem in alternatedMatcher.PatternMatchers[i].Accept ( this ) )
+                    yield return elem;
         }
 
-        public MinimalExpressionNode Visit ( CharMatcher charMatcher )
+        public IEnumerable<StringBuilder> Visit ( CharMatcher charMatcher )
         {
-            return new MinimalExpressionNode ( charMatcher.StringFilter );
+            var list = new StringBuilder ( );
+            list.Insert ( 0, charMatcher.StringFilter );
+            yield return list;
         }
 
-        public MinimalExpressionNode Visit ( RangeMatcher RangeMatcher )
+        public IEnumerable<StringBuilder> Visit ( RangeMatcher rangeMatcher )
         {
-            var range = RangeMatcher.Range;
-            if ( this.Negated )
-            {
-                if ( range.Start > Char.MinValue )
-                    return new MinimalExpressionNode ( ( ( Char ) ( range.Start - 1 ) ).ToString ( ) );
-                else if ( range.End < Char.MaxValue )
-                    return new MinimalExpressionNode ( ( ( Char ) ( range.End + 1 ) ).ToString ( ) );
-                else
-                    throw new InvalidOperationException ( "Cannot find a character outside of this range." );
-            }
-            else
-                return new MinimalExpressionNode ( ( ( Char ) range.Start ).ToString ( ) );
+            Common.Math.Range<Char> range = rangeMatcher.Range;
+            yield return new StringBuilder ( range.Start.ToString ( ) );
         }
 
-        public MinimalExpressionNode Visit ( EOFMatcher eofMatcher )
+        public IEnumerable<StringBuilder> Visit ( EOFMatcher eofMatcher )
         {
-            return this.Negated
-                ? new MinimalExpressionNode ( "a" )
-                // Just needs to be a leaf (it'll be a pain to
-                // check if it is though, so we don't)
-                : MinimalExpressionNode.CreateNew ( );
+            yield return new StringBuilder ( );
         }
 
-        public MinimalExpressionNode Visit ( FilterFuncMatcher filterFuncMatcher )
+        public IEnumerable<StringBuilder> Visit ( FilterFuncMatcher filterFuncMatcher )
         {
             var strs = new List<String> ( );
             for ( var i = Char.MinValue; i <= Char.MaxValue; i++ )
-                if ( this.Negated ^ filterFuncMatcher.Filter ( i ) )
-                    return new MinimalExpressionNode ( i.ToString ( ) );
-
-            throw new InvalidOperationException ( $"Cannot get a minimal expression for a predicate which {( this.Negated ? "" : "doesn't " )}accepts any chars." );
-        }
-
-        public MinimalExpressionNode Visit ( IgnoreMatcher ignoreMatcher )
-        {
-            return ignoreMatcher.PatternMatcher.Accept ( this );
-        }
-
-        public MinimalExpressionNode Visit ( JoinMatcher joinMatcher )
-        {
-            return joinMatcher.PatternMatcher.Accept ( this );
-        }
-
-        public MinimalExpressionNode Visit ( MarkerMatcher markerMatcher )
-        {
-            return markerMatcher.PatternMatcher.Accept ( this );
-        }
-
-        public MinimalExpressionNode Visit ( CharListMatcher charListMatcher )
-        {
-            if ( this.Negated )
             {
-                for ( var i = Char.MinValue; i <= Char.MaxValue; i++ )
-                    if ( Array.IndexOf ( charListMatcher.Whitelist, i ) == -1 )
-                        return new MinimalExpressionNode ( i.ToString ( ) );
+                if ( filterFuncMatcher.Filter ( i ) )
+                {
+                    yield return new StringBuilder ( i.ToString ( ) );
+                    yield break;
+                }
             }
-            return new MinimalExpressionNode ( charListMatcher.Whitelist[0].ToString ( ) );
+
+            throw new InvalidOperationException ( $"Cannot get a minimal expression for a predicate which doesn't accepts any chars." );
         }
 
-        public MinimalExpressionNode Visit ( NegatedMatcher negatedMatcher )
+        public IEnumerable<StringBuilder> Visit ( IgnoreMatcher ignoreMatcher ) => ignoreMatcher.PatternMatcher.Accept ( this );
+
+        public IEnumerable<StringBuilder> Visit ( JoinMatcher joinMatcher ) => joinMatcher.PatternMatcher.Accept ( this );
+
+        public IEnumerable<StringBuilder> Visit ( MarkerMatcher markerMatcher ) => markerMatcher.PatternMatcher.Accept ( this );
+
+        public IEnumerable<StringBuilder> Visit ( CharListMatcher charListMatcher )
         {
-            return negatedMatcher.PatternMatcher.Accept ( new MinimalExpressionsGenerator ( this.Parser, !this.Negated ) );
+            yield return new StringBuilder ( charListMatcher.Whitelist[0].ToString ( ) );
         }
 
-        public MinimalExpressionNode Visit ( OptionalMatcher optionalMatcher )
-        {
-            if ( this.Negated )
-                throw new InvalidOperationException ( "Cannot negate an optional matcher." );
+        public IEnumerable<StringBuilder> Visit ( NegatedMatcher negatedMatcher ) => throw new NotSupportedException ( "Negated matchers are not supported." );
 
-            return new MinimalExpressionNode (
-                // Matches optional
-                optionalMatcher.PatternMatcher.Accept ( this ),
-                // Doesn't matches optional
-                MinimalExpressionNode.CreateNew ( ) );
+        public IEnumerable<StringBuilder> Visit ( OptionalMatcher optionalMatcher )
+        {
+            yield return new StringBuilder ( );
+            foreach ( StringBuilder option in optionalMatcher.PatternMatcher.Accept ( this ) )
+                yield return option;
         }
 
-        public MinimalExpressionNode Visit ( RepeatedMatcher repeatedMatcher )
+        public IEnumerable<StringBuilder> Visit ( RepeatedMatcher repeatedMatcher )
         {
-            // If we can, don't match anything.
+            // If we can, don't insert anything
             if ( repeatedMatcher.Range.Start == 0 )
-                return MinimalExpressionNode.CreateNew ( );
-
-            var flat = repeatedMatcher.PatternMatcher.Accept ( this ).Flatten ( );
-            var str = flat[0];
-            var node = new MinimalExpressionNode ( str );
-            for ( var i = 1; i < repeatedMatcher.Range.Start; i++ )
             {
-                var subNode = new MinimalExpressionNode ( str );
-                node.AddChild ( subNode );
-                node = subNode;
+                yield return new StringBuilder ( );
+                yield break;
             }
-            return node;
+
+            IEnumerable<StringBuilder> innerMatcherExpressions = repeatedMatcher.PatternMatcher.Accept ( this );
+            IEnumerable<StringBuilder> expressions = innerMatcherExpressions;
+            for ( var i = 2; i <= repeatedMatcher.Range.Start; i++ )
+                expressions = CartesianProduct ( expressions, innerMatcherExpressions );
+
+            foreach ( StringBuilder expression in expressions )
+                yield return expression;
         }
 
-        public MinimalExpressionNode Visit ( RulePlaceholder rulePlaceholder )
+        public IEnumerable<StringBuilder> Visit ( RulePlaceholder rulePlaceholder ) => this.Parser.RawRule ( rulePlaceholder.Name ).Accept ( this );
+
+        public IEnumerable<StringBuilder> Visit ( RuleWrapper ruleWrapper ) => ruleWrapper.PatternMatcher.Accept ( this );
+
+        public IEnumerable<StringBuilder> Visit ( StringMatcher stringMatcher )
         {
-            return this.Parser.RawRule ( rulePlaceholder.Name ).Accept ( this );
+            yield return new StringBuilder ( stringMatcher.StringFilter );
         }
 
-        public MinimalExpressionNode Visit ( RuleWrapper ruleWrapper )
-        {
-            return ruleWrapper.PatternMatcher.Accept ( this );
-        }
+        public IEnumerable<StringBuilder> Visit ( SavingMatcher savingMatcher ) => throw new NotSupportedException ( );
 
-        public MinimalExpressionNode Visit ( StringMatcher stringMatcher )
-        {
-            return new MinimalExpressionNode ( this.Negated
-                ? ( ( Char ) ( stringMatcher.StringFilter[0] + 1 ) ) + stringMatcher.StringFilter.Substring ( 1 )
-                : stringMatcher.StringFilter );
-        }
+        public IEnumerable<StringBuilder> Visit ( LoadingMatcher loadingMatcher ) => throw new NotSupportedException ( );
 
-        public MinimalExpressionNode Visit ( SavingMatcher savingMatcher )
-        {
-            throw new NotSupportedException ( );
-        }
-
-        public MinimalExpressionNode Visit ( LoadingMatcher loadingMatcher )
-        {
-            throw new NotSupportedException ( );
-        }
+        #endregion IMatcherTreeVisitor<IEnumerable<StringBuilder>>
     }
 }
