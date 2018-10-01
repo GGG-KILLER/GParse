a1b31657fe73814015980795b6c162d6e89f4f10
GGG KILLER <gggkiller2@gmail.com>
2018-10-01 18:02:11 -0300

    Add a new exception for the new ModularLexer error handling

---
+++ GParse.Parsing/Lexing/Errors/UnableToContinueLexingException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using GParse.Common;
+using GParse.Common.Errors;
+using GParse.Common.IO;
+
+namespace GParse.Parsing.Lexing.Errors
+{
+    public class UnableToContinueLexingException : LocationBasedException
+    {
+        public UnableToContinueLexingException ( SourceLocation location, String message, SourceCodeReader reader ) : base ( location, message )
+        {
+            this.Reader = reader;
+        }
+
+        public UnableToContinueLexingException ( SourceLocation location, String message, SourceCodeReader reader, Exception innerException ) : base ( location, message, innerException )
+        {
+            this.Reader = reader;
+        }
+
+        public SourceCodeReader Reader { get; }
+    }
+}
