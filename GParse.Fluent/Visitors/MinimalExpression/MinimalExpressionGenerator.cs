a2de8eafdbb942329c0187c3fd056aa8c84f69f3
GGG KILLER <gggkiller2@gmail.com>
2018-11-09 00:40:39 -0200

    Make minimal expression generator use strings instead of stringbuilders

--- GParse.Fluent/Visitors/MinimalExpression/MinimalExpressionGenerator.cs
+++ GParse.Fluent/Visitors/MinimalExpression/MinimalExpressionGenerator.cs
@@ -1,23 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using GParse.Fluent.Abstractions;
 using GParse.Fluent.Matchers;
 
 namespace GParse.Fluent.Visitors.MinimalExpression
 {
-    public class MinimalExpressionsGenerator : IMatcherTreeVisitor<IEnumerable<StringBuilder>>
+    public class MinimalExpressionsGenerator : IMatcherTreeVisitor<IEnumerable<String>>
     {
         private readonly FluentParser Parser;
 
         #region Helper funcs
 
-        private static IEnumerable<StringBuilder> CartesianProduct ( IEnumerable<StringBuilder> lhs, IEnumerable<StringBuilder> rhs )
+        private static IEnumerable<String> CartesianProduct ( IEnumerable<String> lhs, IEnumerable<String> rhs )
         {
-            foreach ( StringBuilder a in lhs )
-                foreach ( StringBuilder b in rhs )
-                    yield return new StringBuilder ( a.ToString ( ) + b );
+            foreach ( var a in lhs )
+                foreach ( var b in rhs )
+                    yield return a + b;
         }
 
         #endregion Helper funcs
@@ -27,52 +26,47 @@ namespace GParse.Fluent.Visitors.MinimalExpression
             this.Parser = parser;
         }
 
-        public String[] Generate ( BaseMatcher matcher ) =>
-            matcher.Accept ( this ).Select ( builder => builder.ToString ( ) ).ToArray ( );
+        public String[] Generate ( BaseMatcher matcher ) => matcher.Accept ( this ).ToArray ( );
 
-        #region IMatcherTreeVisitor<IEnumerable<StringBuilder>>
+        #region IMatcherTreeVisitor<IEnumerable<String>>
 
-        public IEnumerable<StringBuilder> Visit ( SequentialMatcher sequentialMatcher )
+        public IEnumerable<String> Visit ( SequentialMatcher sequentialMatcher )
         {
-            IEnumerable<StringBuilder> last = sequentialMatcher.PatternMatchers[0].Accept ( this );
+            IEnumerable<String> last = sequentialMatcher.PatternMatchers[0].Accept ( this );
             for ( var i = 1; i < sequentialMatcher.PatternMatchers.Length; i++ )
                 last = CartesianProduct ( last, sequentialMatcher.PatternMatchers[i].Accept ( this ) );
             return last;
         }
 
-        public IEnumerable<StringBuilder> Visit ( AlternatedMatcher alternatedMatcher )
+        public IEnumerable<String> Visit ( AlternatedMatcher alternatedMatcher )
         {
             for ( var i = 0; i < alternatedMatcher.PatternMatchers.Length; i++ )
-                foreach ( StringBuilder elem in alternatedMatcher.PatternMatchers[i].Accept ( this ) )
+                foreach ( var elem in alternatedMatcher.PatternMatchers[i].Accept ( this ) )
                     yield return elem;
         }
 
-        public IEnumerable<StringBuilder> Visit ( CharMatcher charMatcher )
+        public IEnumerable<String> Visit ( CharMatcher charMatcher )
         {
-            var list = new StringBuilder ( );
-            list.Insert ( 0, charMatcher.StringFilter );
-            yield return list;
+            yield return charMatcher.StringFilter;
         }
 
-        public IEnumerable<StringBuilder> Visit ( RangeMatcher rangeMatcher )
+        public IEnumerable<String> Visit ( RangeMatcher rangeMatcher )
         {
-            Common.Math.Range<Char> range = rangeMatcher.Range;
-            yield return new StringBuilder ( range.Start.ToString ( ) );
+            yield return rangeMatcher.Range.Start.ToString ( );
         }
 
-        public IEnumerable<StringBuilder> Visit ( EOFMatcher eofMatcher )
+        public IEnumerable<String> Visit ( EOFMatcher eofMatcher )
         {
-            yield return new StringBuilder ( );
+            yield return "";
         }
 
-        public IEnumerable<StringBuilder> Visit ( FilterFuncMatcher filterFuncMatcher )
+        public IEnumerable<String> Visit ( FilterFuncMatcher filterFuncMatcher )
         {
-            var strs = new List<String> ( );
             for ( var i = Char.MinValue; i <= Char.MaxValue; i++ )
             {
                 if ( filterFuncMatcher.Filter ( i ) )
                 {
-                    yield return new StringBuilder ( i.ToString ( ) );
+                    yield return i.ToString ( );
                     yield break;
                 }
             }
@@ -80,57 +74,57 @@ namespace GParse.Fluent.Visitors.MinimalExpression
             throw new InvalidOperationException ( $"Cannot get a minimal expression for a predicate which doesn't accepts any chars." );
         }
 
-        public IEnumerable<StringBuilder> Visit ( IgnoreMatcher ignoreMatcher ) => ignoreMatcher.PatternMatcher.Accept ( this );
+        public IEnumerable<String> Visit ( IgnoreMatcher ignoreMatcher ) => ignoreMatcher.PatternMatcher.Accept ( this );
 
-        public IEnumerable<StringBuilder> Visit ( JoinMatcher joinMatcher ) => joinMatcher.PatternMatcher.Accept ( this );
+        public IEnumerable<String> Visit ( JoinMatcher joinMatcher ) => joinMatcher.PatternMatcher.Accept ( this );
 
-        public IEnumerable<StringBuilder> Visit ( MarkerMatcher markerMatcher ) => markerMatcher.PatternMatcher.Accept ( this );
+        public IEnumerable<String> Visit ( MarkerMatcher markerMatcher ) => markerMatcher.PatternMatcher.Accept ( this );
 
-        public IEnumerable<StringBuilder> Visit ( CharListMatcher charListMatcher )
+        public IEnumerable<String> Visit ( CharListMatcher charListMatcher )
         {
-            yield return new StringBuilder ( charListMatcher.Whitelist[0].ToString ( ) );
+            yield return charListMatcher.Whitelist[0].ToString ( );
         }
 
-        public IEnumerable<StringBuilder> Visit ( NegatedMatcher negatedMatcher ) => throw new NotSupportedException ( "Negated matchers are not supported." );
+        public IEnumerable<String> Visit ( NegatedMatcher negatedMatcher ) => throw new NotSupportedException ( "Negated matchers are not supported." );
 
-        public IEnumerable<StringBuilder> Visit ( OptionalMatcher optionalMatcher )
+        public IEnumerable<String> Visit ( OptionalMatcher optionalMatcher )
         {
-            yield return new StringBuilder ( );
-            foreach ( StringBuilder option in optionalMatcher.PatternMatcher.Accept ( this ) )
+            yield return "";
+            foreach ( var option in optionalMatcher.PatternMatcher.Accept ( this ) )
                 yield return option;
         }
 
-        public IEnumerable<StringBuilder> Visit ( RepeatedMatcher repeatedMatcher )
+        public IEnumerable<String> Visit ( RepeatedMatcher repeatedMatcher )
         {
             // If we can, don't insert anything
             if ( repeatedMatcher.Range.Start == 0 )
             {
-                yield return new StringBuilder ( );
+                yield return "";
                 yield break;
             }
 
-            IEnumerable<StringBuilder> innerMatcherExpressions = repeatedMatcher.PatternMatcher.Accept ( this );
-            IEnumerable<StringBuilder> expressions = innerMatcherExpressions;
+            IEnumerable<String> innerMatcherExpressions = repeatedMatcher.PatternMatcher.Accept ( this );
+            IEnumerable<String> expressions = innerMatcherExpressions;
             for ( var i = 2; i <= repeatedMatcher.Range.Start; i++ )
                 expressions = CartesianProduct ( expressions, innerMatcherExpressions );
 
-            foreach ( StringBuilder expression in expressions )
+            foreach ( var expression in expressions )
                 yield return expression;
         }
 
-        public IEnumerable<StringBuilder> Visit ( RulePlaceholder rulePlaceholder ) => this.Parser.RawRule ( rulePlaceholder.Name ).Accept ( this );
+        public IEnumerable<String> Visit ( RulePlaceholder rulePlaceholder ) => this.Parser.RawRule ( rulePlaceholder.Name ).Accept ( this );
 
-        public IEnumerable<StringBuilder> Visit ( RuleWrapper ruleWrapper ) => ruleWrapper.PatternMatcher.Accept ( this );
+        public IEnumerable<String> Visit ( RuleWrapper ruleWrapper ) => ruleWrapper.PatternMatcher.Accept ( this );
 
-        public IEnumerable<StringBuilder> Visit ( StringMatcher stringMatcher )
+        public IEnumerable<String> Visit ( StringMatcher stringMatcher )
         {
-            yield return new StringBuilder ( stringMatcher.StringFilter );
+            yield return stringMatcher.StringFilter;
         }
 
-        public IEnumerable<StringBuilder> Visit ( SavingMatcher savingMatcher ) => throw new NotSupportedException ( );
+        public IEnumerable<String> Visit ( SavingMatcher savingMatcher ) => throw new NotSupportedException ( );
 
-        public IEnumerable<StringBuilder> Visit ( LoadingMatcher loadingMatcher ) => throw new NotSupportedException ( );
+        public IEnumerable<String> Visit ( LoadingMatcher loadingMatcher ) => throw new NotSupportedException ( );
 
-        #endregion IMatcherTreeVisitor<IEnumerable<StringBuilder>>
+        #endregion IMatcherTreeVisitor<IEnumerable<String>>
     }
 }
