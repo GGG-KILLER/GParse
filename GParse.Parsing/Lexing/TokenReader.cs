844f9480990559b422a667dfd9b7b49fed7be100
GGG KILLER <gggkiller2@gmail.com>
2018-11-29 20:00:28 -0200

    Add more helper methods to ITokenReader

--- GParse.Parsing/Lexing/TokenReader.cs
+++ GParse.Parsing/Lexing/TokenReader.cs
@@ -42,7 +42,11 @@ namespace GParse.Parsing.Lexing
             this.TokenCache = new List<Token<TokenTypeT>> ( maxLookaheadOffset );
         }
 
-        private void CacheTokens ( Int32 count )
+        /// <summary>
+        /// Saves N tokens from the lexer on the readahead cache
+        /// </summary>
+        /// <param name="count"></param>
+        protected void CacheTokens ( Int32 count )
         {
             while ( count-- > 0 )
                 this.TokenCache.Add ( this.Lexer.Consume ( ) );
@@ -117,6 +121,76 @@ namespace GParse.Parsing.Lexing
             }
         }
 
+        #region IsAhead
+
+        /// <summary>
+        /// <inheritdoc />
+        /// </summary>
+        /// <param name="tokenType">The wanted type</param>
+        /// <param name="offset">The offset</param>
+        /// <returns></returns>
+        public Boolean IsAhead ( TokenTypeT tokenType, Int32 offset = 0 ) => this.Lookahead ( offset ).Type.Equals ( tokenType );
+
+        /// <summary>
+        /// <inheritdoc />
+        /// </summary>
+        /// <param name="tokenTypes"></param>
+        /// <param name="offset"></param>
+        /// <returns></returns>
+        public Boolean IsAhead ( IEnumerable<TokenTypeT> tokenTypes, Int32 offset = 0 )
+        {
+            TokenTypeT type = this.Lookahead(offset).Type;
+            foreach ( TokenTypeT wtype in tokenTypes )
+                if ( wtype.Equals ( type ) )
+                    return true;
+            return false;
+        }
+
+        /// <summary>
+        /// <inheritdoc />
+        /// </summary>
+        /// <param name="ID"></param>
+        /// <param name="offset"></param>
+        /// <returns></returns>
+        public Boolean IsAhead ( String ID, Int32 offset = 0 ) => this.Lookahead ( offset ).ID.Equals ( ID );
+
+        /// <summary>
+        /// <inheritdoc />
+        /// </summary>
+        /// <param name="ids"></param>
+        /// <param name="offset"></param>
+        /// <returns></returns>
+        public Boolean IsAhead ( IEnumerable<String> ids, Int32 offset = 0 )
+        {
+            var aheadId = this.Lookahead ( offset ).ID;
+            foreach ( var id in ids )
+                if ( id.Equals ( aheadId ) )
+                    return true;
+            return false;
+        }
+
+        /// <summary>
+        /// <inheritdoc />
+        /// </summary>
+        /// <param name="tokenType"></param>
+        /// <param name="id"></param>
+        /// <param name="offset"></param>
+        /// <returns></returns>
+        public Boolean IsAhead ( TokenTypeT tokenType, String id, Int32 offset = 0 ) =>
+            this.IsAhead ( tokenType, offset ) && this.IsAhead ( id, offset );
+
+        /// <summary>
+        /// <inheritdoc />
+        /// </summary>
+        /// <param name="tokenTypes"></param>
+        /// <param name="ids"></param>
+        /// <param name="offset"></param>
+        /// <returns></returns>
+        public Boolean IsAhead ( IEnumerable<TokenTypeT> tokenTypes, IEnumerable<String> ids, Int32 offset = 0 ) =>
+            this.IsAhead ( tokenTypes, offset ) && this.IsAhead ( ids, offset );
+
+        #endregion IsAhead
+
         #region Accept
 
         /// <summary>
@@ -127,7 +201,24 @@ namespace GParse.Parsing.Lexing
         /// <returns></returns>
         public Boolean Accept ( String ID, out Token<TokenTypeT> token )
         {
-            if ( this.Lookahead ( ).ID == ID )
+            if ( this.IsAhead ( ID ) )
+            {
+                token = this.Consume ( );
+                return true;
+            }
+            token = null;
+            return false;
+        }
+
+        /// <summary>
+        /// <inheritdoc />
+        /// </summary>
+        /// <param name="IDs"></param>
+        /// <param name="token"></param>
+        /// <returns></returns>
+        public Boolean Accept ( IEnumerable<String> IDs, out Token<TokenTypeT> token )
+        {
+            if ( this.IsAhead ( IDs ) )
             {
                 token = this.Consume ( );
                 return true;
@@ -146,12 +237,36 @@ namespace GParse.Parsing.Lexing
         /// <summary>
         /// <inheritdoc />
         /// </summary>
+        /// <param name="IDs"></param>
+        /// <returns></returns>
+        public Boolean Accept ( IEnumerable<String> IDs ) => this.Accept ( IDs, out _ );
+
+        /// <summary>
+        /// <inheritdoc />
+        /// </summary>
         /// <param name="type"></param>
         /// <param name="token"></param>
         /// <returns></returns>
         public Boolean Accept ( TokenTypeT type, out Token<TokenTypeT> token )
         {
-            if ( this.Lookahead ( ).Type.Equals ( type ) )
+            if ( this.IsAhead ( type ) )
+            {
+                token = this.Consume ( );
+                return true;
+            }
+            token = null;
+            return false;
+        }
+
+        /// <summary>
+        /// <inheritdoc />
+        /// </summary>
+        /// <param name="types"></param>
+        /// <param name="token"></param>
+        /// <returns></returns>
+        public Boolean Accept ( IEnumerable<TokenTypeT> types, out Token<TokenTypeT> token )
+        {
+            if ( this.IsAhead ( types ) )
             {
                 token = this.Consume ( );
                 return true;
@@ -170,13 +285,38 @@ namespace GParse.Parsing.Lexing
         /// <summary>
         /// <inheritdoc />
         /// </summary>
+        /// <param name="types"></param>
+        /// <returns></returns>
+        public Boolean Accept ( IEnumerable<TokenTypeT> types ) => this.Accept ( types, out _ );
+
+        /// <summary>
+        /// <inheritdoc />
+        /// </summary>
         /// <param name="ID"></param>
         /// <param name="type"></param>
         /// <param name="token"></param>
         /// <returns></returns>
         public Boolean Accept ( String ID, TokenTypeT type, out Token<TokenTypeT> token )
         {
-            if ( this.Lookahead ( ).Type.Equals ( type ) && this.Lookahead ( ).ID == ID )
+            if ( this.IsAhead ( type, ID ) )
+            {
+                token = this.Consume ( );
+                return true;
+            }
+            token = null;
+            return false;
+        }
+
+        /// <summary>
+        /// <inheritdoc />
+        /// </summary>
+        /// <param name="IDs"></param>
+        /// <param name="types"></param>
+        /// <param name="token"></param>
+        /// <returns></returns>
+        public Boolean Accept ( IEnumerable<String> IDs, IEnumerable<TokenTypeT> types, out Token<TokenTypeT> token )
+        {
+            if ( this.IsAhead ( types, IDs ) )
             {
                 token = this.Consume ( );
                 return true;
@@ -193,6 +333,14 @@ namespace GParse.Parsing.Lexing
         /// <returns></returns>
         public Boolean Accept ( String ID, TokenTypeT type ) => this.Accept ( ID, type, out _ );
 
+        /// <summary>
+        /// <inheritdoc />
+        /// </summary>
+        /// <param name="IDs"></param>
+        /// <param name="types"></param>
+        /// <returns></returns>
+        public Boolean Accept ( IEnumerable<String> IDs, IEnumerable<TokenTypeT> types ) => this.Accept ( IDs, types, out _ );
+
         #endregion Accept
 
         #region Expect
@@ -204,10 +352,23 @@ namespace GParse.Parsing.Lexing
         /// <returns></returns>
         public Token<TokenTypeT> Expect ( String ID )
         {
-            Token<TokenTypeT> peek = this.Lookahead ( );
-            if ( peek == null || peek.ID != ID )
-                throw new ParsingException ( peek?.Range.Start ?? SourceLocation.Min, $"Expected a {ID} but got {peek?.ID ?? "EOF"} instead." );
-            return this.Consume ( );
+            Token<TokenTypeT> next = this.Lookahead ( );
+            if ( !this.Accept ( ID ) )
+                throw new ParsingException ( next?.Range.Start ?? SourceLocation.Min, $"Expected a {ID} but got {next?.ID ?? "unknown"} instead." );
+            return next;
+        }
+
+        /// <summary>
+        /// <inheritdoc />
+        /// </summary>
+        /// <param name="IDs"></param>
+        /// <returns></returns>
+        public Token<TokenTypeT> Expect ( IEnumerable<String> IDs )
+        {
+            Token<TokenTypeT> next = this.Lookahead ( );
+            if ( !this.Accept ( IDs ) )
+                throw new ParsingException ( next?.Range.Start ?? SourceLocation.Min, $"Expected any ({String.Join ( ", ", IDs )}) but got {next?.ID ?? "unknown"}" );
+            return next;
         }
 
         /// <summary>
@@ -217,10 +378,23 @@ namespace GParse.Parsing.Lexing
         /// <returns></returns>
         public Token<TokenTypeT> Expect ( TokenTypeT type )
         {
-            Token<TokenTypeT> peek = this.Lookahead ( );
-            if ( peek == null || !peek.Type.Equals ( type ) )
-                throw new ParsingException ( peek?.Range.Start ?? SourceLocation.Min, $"Expected a {type} but got {peek?.Type.ToString ( ) ?? "EOF"} instead." );
-            return this.Consume ( );
+            Token<TokenTypeT> next = this.Lookahead ( );
+            if ( !this.Accept ( type ) )
+                throw new ParsingException ( next?.Range.Start ?? SourceLocation.Min, $"Expected a {type} but got {next?.Type.ToString ( ) ?? "EOF"} instead." );
+            return next;
+        }
+
+        /// <summary>
+        /// <inheritdoc />
+        /// </summary>
+        /// <param name="types"></param>
+        /// <returns></returns>
+        public Token<TokenTypeT> Expect ( IEnumerable<TokenTypeT> types )
+        {
+            Token<TokenTypeT> next = this.Lookahead ( );
+            if ( !this.Accept ( types ) )
+                throw new ParsingException ( next?.Range.Start ?? SourceLocation.Min, $"Expected any ({String.Join ( ", ", types )}) but got {next.Type}" );
+            return next;
         }
 
         /// <summary>
@@ -231,10 +405,24 @@ namespace GParse.Parsing.Lexing
         /// <returns></returns>
         public Token<TokenTypeT> Expect ( String ID, TokenTypeT type )
         {
-            Token<TokenTypeT> peek = this.Lookahead ( );
-            if ( peek == null || !peek.Type.Equals ( type ) || peek.ID != ID )
-                throw new ParsingException ( peek?.Range.Start ?? SourceLocation.Min, $"Expected a {ID}+{type} but got a {peek?.ID ?? "EOF"}+{peek?.Type.ToString ( ) ?? "EOF"}" );
-            return this.Consume ( );
+            Token<TokenTypeT> next = this.Lookahead ( );
+            if ( !this.Accept ( ID, type ) )
+                throw new ParsingException ( next?.Range.Start ?? SourceLocation.Min, $"Expected a {ID}+{type} but got a {next?.ID ?? "EOF"}+{next?.Type.ToString ( ) ?? "EOF"}" );
+            return next;
+        }
+
+        /// <summary>
+        /// <inheritdoc />
+        /// </summary>
+        /// <param name="IDs"></param>
+        /// <param name="types"></param>
+        /// <returns></returns>
+        public Token<TokenTypeT> Expect ( IEnumerable<String> IDs, IEnumerable<TokenTypeT> types )
+        {
+            Token<TokenTypeT> next = this.Lookahead ( );
+            if ( !this.Accept ( IDs, types ) )
+                throw new ParsingException ( next?.Range.Start ?? SourceLocation.Min, $"Expected any ({String.Join ( ", ", IDs )})+({String.Join ( ", ", types )}) but got {next?.ID ?? "unknown"}+{next?.Type.ToString ( ) ?? "unknown"}" );
+            return next;
         }
 
         #endregion Expect
