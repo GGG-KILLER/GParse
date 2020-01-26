0be483345166bcb8a677ba551d65e0b5db8ead8e
GGG KILLER <gggkiller2@gmail.com>
2020-01-26 17:38:57 -0300

    Initial work on composable lexers

---
+++ GParse/Lexing/Composable/ComposableRuleCompiler.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+using System.Text;
+using GParse.Composable;
+using GParse.Lexing.Modules;
+
+namespace GParse.Lexing.Composable
+{
+    public class ComposableRuleCompiler<TTokenType> : IComposableRuleCompiler<TTokenType>
+    {
+        public ILexerModule<TTokenType> CompileRule ( GrammarNode<Char> grammarNode )
+        {
+            throw new NotImplementedException ( );
+        }
+    }
+}
