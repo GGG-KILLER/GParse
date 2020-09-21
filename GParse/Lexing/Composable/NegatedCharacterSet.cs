3094734676d34951177c84cfc1faa2dea1d490db
GGG KILLER <gggkiller2@gmail.com>
2020-09-21 20:24:26 -0300

    Don't inherit from CharacterSet and implement ! operator and tostring.

--- GParse/Lexing/Composable/NegatedCharacterSet.cs
+++ GParse/Lexing/Composable/NegatedCharacterSet.cs
@@ -1,18 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using GParse.Composable;
 
 namespace GParse.Lexing.Composable
 {
     /// <summary>
     /// A node that matches anything that is not in this character set
     /// </summary>
-    public class NegatedCharacterSet : CharacterSet
+    public class NegatedCharacterSet : GrammarNode<Char>
     {
+        /// <summary>
+        /// The character set.
+        /// </summary>
+        public IImmutableSet<Char> CharSet { get; }
+
         /// <summary>
         /// Initializes this negated character set
         /// </summary>
         /// <param name="charSet"></param>
-        public NegatedCharacterSet ( params Char[] charSet ) : base ( charSet )
+        public NegatedCharacterSet ( params Char[] charSet ) : this ( ( IEnumerable<Char> ) charSet )
         {
         }
 
@@ -20,8 +28,24 @@ namespace GParse.Lexing.Composable
         /// Initializes this negated character set
         /// </summary>
         /// <param name="charSet"></param>
-        public NegatedCharacterSet ( ISet<Char> charSet ) : base ( charSet )
+        public NegatedCharacterSet ( IEnumerable<Char> charSet )
         {
+            this.CharSet = charSet.ToImmutableHashSet ( );
         }
+
+        /// <summary>
+        /// Negates a negated character set.
+        /// </summary>
+        /// <param name="negatedCharacterSet"></param>
+        /// <returns></returns>
+        public static CharacterSet operator ! ( NegatedCharacterSet negatedCharacterSet ) =>
+            new CharacterSet ( negatedCharacterSet.CharSet );
+
+        /// <summary>
+        /// Converts this node back into a regex string.
+        /// </summary>
+        /// <returns></returns>
+        public override String ToString ( ) =>
+            $"[^{String.Join ( "", this.CharSet.Select ( ch => CharUtils.ToReadableString ( ch ) ) )}]";
     }
 }
