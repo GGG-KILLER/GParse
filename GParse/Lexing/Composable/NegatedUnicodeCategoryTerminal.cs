6a9771656b05a428865a065e7327e5df4c0ebd0e
GGG KILLER <gggkiller2@gmail.com>
2020-09-21 20:25:49 -0300

    Add a unicode category node.

---
+++ GParse/Lexing/Composable/NegatedUnicodeCategoryTerminal.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+using GParse.Composable;
+
+namespace GParse.Lexing.Composable
+{
+    /// <summary>
+    /// Represents a negated unicode category terminal.
+    /// </summary>
+    public class NegatedUnicodeCategoryTerminal : GrammarNode<Char>
+    {
+        /// <summary>
+        /// The unicode category.
+        /// </summary>
+        public UnicodeCategory Category { get; }
+
+        /// <summary>
+        /// Initializes a new negated unicode category terminal.
+        /// </summary>
+        /// <param name="category"></param>
+        public NegatedUnicodeCategoryTerminal ( UnicodeCategory category )
+        {
+            this.Category = category;
+        }
+
+        /// <summary>
+        /// Negated a negated unicode category terminal.
+        /// </summary>
+        /// <param name="negatedCategoryTerminal"></param>
+        /// <returns></returns>
+        public static UnicodeCategoryTerminal operator ! ( NegatedUnicodeCategoryTerminal negatedCategoryTerminal ) =>
+            new UnicodeCategoryTerminal ( negatedCategoryTerminal.Category );
+    }
+}
---
+++ GParse/Lexing/Composable/UnicodeCategoryTerminal.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+using GParse.Composable;
+
+namespace GParse.Lexing.Composable
+{
+    /// <summary>
+    /// A unicode category terminal.
+    /// </summary>
+    public class UnicodeCategoryTerminal : GrammarNode<Char>
+    {
+        /// <summary>
+        /// The unicode category.
+        /// </summary>
+        public UnicodeCategory Category { get; }
+
+        /// <summary>
+        /// Initializes a new unicode category terminal.
+        /// </summary>
+        /// <param name="category"></param>
+        public UnicodeCategoryTerminal ( UnicodeCategory category )
+        {
+            this.Category = category;
+        }
+
+        /// <summary>
+        /// Negates a unicode category terminal.
+        /// </summary>
+        /// <param name="categoryTerminal"></param>
+        /// <returns></returns>
+        public static NegatedUnicodeCategoryTerminal operator ! ( UnicodeCategoryTerminal categoryTerminal ) =>
+            new NegatedUnicodeCategoryTerminal ( categoryTerminal.Category );
+    }
+}
