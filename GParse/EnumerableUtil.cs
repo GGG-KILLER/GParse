463e739f38310e259c94fd36278277d15bed0bf3
GGG KILLER <gggkiller2@gmail.com>
2020-09-21 12:50:33 -0300

    Add enumerable utility class

---
+++ GParse/EnumerableUtil.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace GParse
+{
+    internal static class EnumerableUtil
+    {
+        /// <summary>
+        /// Yields a single value as an enumerable.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static IEnumerable<T> Yield<T> ( T value )
+        {
+            yield return value;
+        }
+    }
+}
