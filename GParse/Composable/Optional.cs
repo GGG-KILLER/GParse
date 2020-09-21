54877a1a6cc78675b51f3dde2a76cca0badcbd02
GGG KILLER <gggkiller2@gmail.com>
2020-09-21 12:52:47 -0300

    Add an optional node.

---
+++ GParse/Composable/Optional.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace GParse.Composable
+{
+    /// <summary>
+    /// Represents an optional grammar node.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class Optional<T> : GrammarNodeContainer<T>
+    {
+        /// <summary>
+        /// Initializes a new optional node.
+        /// </summary>
+        /// <param name="innerNode"><inheritdoc cref="InnerNode" path="/summary" /></param>
+        public Optional ( GrammarNode<T> innerNode )
+            : base ( innerNode is Optional<T> optional ? optional.InnerNode : innerNode )
+        {
+        }
+    }
+
+    /// <summary>
+    /// A class containing extension methods for grammar nodes.
+    /// </summary>
+    public partial class GrammarNodeExtensions
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="node"></param>
+        /// <returns></returns>
+        [return: NotNullIfNotNull ( "node" )]
+        public static Optional<T>? Optional<T> ( GrammarNode<T>? node ) =>
+            node is null ? null : new Optional<T> ( node );
+    }
+}
