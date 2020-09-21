d63d47962e4559035a93f1b320470c461abeab0d
GGG KILLER <gggkiller2@gmail.com>
2020-09-21 20:22:24 -0300

    Add a lookahead node.

---
+++ GParse/Lexing/Composable/Lookahead.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using GParse.Composable;
+
+namespace GParse.Lexing.Composable
+{
+    /// <summary>
+    /// Represents a lookahead.
+    /// </summary>
+    public class Lookahead : GrammarNodeContainer<Char>
+    {
+        /// <summary>
+        /// Initializes a new lookahead.
+        /// </summary>
+        /// <param name="node"></param>
+        public Lookahead ( GrammarNode<Char> node ) : base ( node )
+        {
+        }
+
+        /// <summary>
+        /// Negates a lookahead.
+        /// </summary>
+        /// <param name="lookahead"></param>
+        /// <returns></returns>
+        public static NegativeLookahead operator ! ( Lookahead lookahead ) =>
+            new NegativeLookahead ( lookahead.InnerNode );
+
+        /// <summary>
+        /// Converts this node back into a regex string.
+        /// </summary>
+        /// <returns></returns>
+        public override String ToString ( ) =>
+            $"(?={GrammarNodeToStringConverter.Convert ( this.InnerNode )})";
+    }
+}
---
+++ GParse/Lexing/Composable/NegativeLookahead.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GParse.Composable;
+
+namespace GParse.Lexing.Composable
+{
+    /// <summary>
+    /// Represents a negative lookahead.
+    /// </summary>
+    public class NegativeLookahead : GrammarNodeContainer<Char>
+    {
+        /// <summary>
+        /// Initializes a new negative lookahead.
+        /// </summary>
+        /// <param name="innerNode"></param>
+        public NegativeLookahead ( GrammarNode<Char> innerNode ) : base ( innerNode )
+        {
+        }
+
+        /// <summary>
+        /// Negates this lookahead.
+        /// </summary>
+        /// <param name="negativeLookahead"></param>
+        /// <returns></returns>
+        public static Lookahead operator ! ( NegativeLookahead negativeLookahead ) =>
+            new Lookahead ( negativeLookahead.InnerNode );
+
+        /// <summary>
+        /// Converts this node back into a regex string.
+        /// </summary>
+        /// <returns></returns>
+        public override String ToString ( ) =>
+            $"(?!{GrammarNodeToStringConverter.Convert ( this.InnerNode )})";
+    }
+}
