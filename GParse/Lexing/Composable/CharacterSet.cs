0236fc066d7d7b4277f7c5351844d4df28641cb2
GGG KILLER <gggkiller2@gmail.com>
2020-09-21 20:21:31 -0300

    Use immutable collection and implement tostring.

--- GParse/Lexing/Composable/CharacterSet.cs
+++ GParse/Lexing/Composable/CharacterSet.cs
@@ -1,40 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Immutable;
 using System.Linq;
 using GParse.Composable;
 
 namespace GParse.Lexing.Composable
 {
     /// <summary>
-    /// A node that matches a set of characters
+    /// A node that matches a set of characters.
     /// </summary>
     public class CharacterSet : GrammarNode<Char>
     {
         /// <summary>
-        /// The set of characters this node matches
+        /// The set of characters this node matches.
         /// </summary>
-        public ISet<Char> CharSet { get; }
+        public IImmutableSet<Char> CharSet { get; }
 
         /// <summary>
-        /// Initializes this character set
+        /// Initializes this character set.
         /// </summary>
         /// <param name="charSet"></param>
-        public CharacterSet ( params Char[] charSet ) : this ( new HashSet<Char> ( charSet ) )
+        public CharacterSet ( params Char[] charSet ) : this ( ( IEnumerable<Char> ) charSet )
         {
         }
 
         /// <summary>
-        /// Initializes this character set
+        /// Initializes this character set.
         /// </summary>
         /// <param name="charSet"></param>
-        public CharacterSet ( ISet<Char> charSet )
+        public CharacterSet ( IEnumerable<Char> charSet )
         {
             if ( charSet is null )
                 throw new ArgumentNullException ( nameof ( charSet ) );
-            if ( charSet.Count < 1 )
+            if ( !charSet.Any ( ) )
                 throw new ArgumentException ( "The input charset must not be empty" );
 
-            this.CharSet = charSet;
+            this.CharSet = charSet.ToImmutableHashSet ( );
         }
 
         /// <summary>
@@ -51,5 +52,12 @@ namespace GParse.Lexing.Composable
         /// <returns></returns>
         public static NegatedCharacterSet operator ! ( CharacterSet charSet ) =>
             new NegatedCharacterSet ( charSet.CharSet );
+
+        /// <summary>
+        /// Tranforms this node back into a regex string.
+        /// </summary>
+        /// <returns></returns>
+        public override String ToString ( ) =>
+            $"[{String.Join ( "", this.CharSet.Select ( ch => CharUtils.ToReadableString ( ch ) ) )}]";
     }
 }
