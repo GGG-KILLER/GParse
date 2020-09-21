c316d0284feeefdb6b1c048b1de1bd08c79f2b27
GGG KILLER <gggkiller2@gmail.com>
2020-09-21 20:26:26 -0300

    Add SimpleMatch and a delegate that returns it.

---
+++ GParse/Lexing/Composable/SimpleMatch.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace GParse.Lexing.Composable
+{
+    /// <summary>
+    /// A simple match containing only whether the match was successful and its length if successful.
+    /// </summary>
+    public readonly struct SimpleMatch
+    {
+        /// <summary>
+        /// Whether this match was successful.
+        /// </summary>
+        public Boolean IsMatch { get; }
+
+        /// <summary>
+        /// The length of the match.
+        /// </summary>
+        public Int32 Length { get; }
+
+        /// <summary>
+        /// Initializes a new simple match.
+        /// </summary>
+        /// <param name="isMatch"><inheritdoc cref="IsMatch" path="/summary" /></param>
+        /// <param name="length"><inheritdoc cref="Length" path="/summary" /></param>
+        public SimpleMatch ( Boolean isMatch, Int32 length )
+        {
+            this.IsMatch = isMatch;
+            this.Length = length;
+        }
+
+        /// <summary>
+        /// Deconstructs this simple match into its counterparts.
+        /// </summary>
+        /// <param name="isMatch"><inheritdoc cref="IsMatch" path="/summary" /></param>
+        /// <param name="length"><inheritdoc cref="Length" path="/summary" /></param>
+        public void Deconstruct ( out Boolean isMatch, out Int32 length ) =>
+            (isMatch, length) = (this.IsMatch, this.Length);
+    }
+}
---
+++ GParse/Lexing/Composable/SimpleMatchDelegate.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using GParse.IO;
+
+namespace GParse.Lexing.Composable
+{
+    /// <summary>
+    /// The delegate that represents the implementation of a grammar node's match action.
+    /// </summary>
+    /// <param name="reader">The reader to use.</param>
+    /// <param name="offset">The offset to start matching at.</param>
+    /// <param name="captures">The dictionary containing all captures.</param>
+    /// <returns></returns>
+    public delegate SimpleMatch SimpleMatchDelegate ( IReadOnlyCodeReader reader,
+                                                      Int32 offset,
+                                                      IDictionary<String, Capture>? captures = null );
+}
