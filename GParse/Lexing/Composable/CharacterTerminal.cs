863e6ae7a3590d1662e0c21e77dd9774a7c32352
GGG KILLER <gggkiller2@gmail.com>
2020-09-21 20:21:45 -0300

    Implement tostring.

--- GParse/Lexing/Composable/CharacterTerminal.cs
+++ GParse/Lexing/Composable/CharacterTerminal.cs
@@ -4,12 +4,12 @@ using GParse.Composable;
 namespace GParse.Lexing.Composable
 {
     /// <summary>
-    /// A character terminal node
+    /// A character terminal node.
     /// </summary>
     public class CharacterTerminal : Terminal<Char>
     {
         /// <summary>
-        /// Initializes this character terminal node
+        /// Initializes this character terminal node.
         /// </summary>
         /// <param name="value"></param>
         public CharacterTerminal ( Char value ) : base ( value )
@@ -17,18 +17,25 @@ namespace GParse.Lexing.Composable
         }
 
         /// <summary>
-        /// The implicit conversion operator of a char to a char terminal
+        /// Converts a char to a char terminal.
         /// </summary>
         /// <param name="ch"></param>
         public static implicit operator CharacterTerminal ( Char ch ) =>
             new CharacterTerminal ( ch );
 
         /// <summary>
-        /// Negates this char terminal to match nodes that are not this char
+        /// Negates this char terminal to match nodes that are not this char.
         /// </summary>
         /// <param name="charTerminal"></param>
         /// <returns></returns>
         public static NegatedCharacterTerminal operator ! ( CharacterTerminal charTerminal ) =>
             new NegatedCharacterTerminal ( charTerminal.Value );
+
+        /// <summary>
+        /// Converts this terminal into a readable character string.
+        /// </summary>
+        /// <returns></returns>
+        public override String ToString ( ) =>
+            CharUtils.ToReadableString ( this.Value );
     }
 }
