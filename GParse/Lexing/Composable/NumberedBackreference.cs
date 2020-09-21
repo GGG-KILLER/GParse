042b05cbf2d7a302f414cfcbb79a6932fcddb62c
GGG KILLER <gggkiller2@gmail.com>
2020-09-21 20:24:55 -0300

    Add position-based capture.

---
+++ GParse/Lexing/Composable/NumberedBackreference.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using GParse.Composable;
+
+namespace GParse.Lexing.Composable
+{
+    /// <summary>
+    /// A numbered backreference.
+    /// </summary>
+    public class NumberedBackreference : GrammarNode<Char>
+    {
+        /// <summary>
+        /// The group number.
+        /// </summary>
+        public Int32 Position { get; }
+
+        /// <summary>
+        /// Initializes a new numbered backreference
+        /// </summary>
+        /// <param name="position"></param>
+        public NumberedBackreference ( Int32 position )
+        {
+            if ( position <= 0 )
+                throw new ArgumentOutOfRangeException ( nameof ( position ) );
+
+            this.Position = position;
+        }
+    }
+}
---
+++ GParse/Lexing/Composable/NumberedCapture.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using GParse.Composable;
+
+namespace GParse.Lexing.Composable
+{
+    /// <summary>
+    /// Represents a numbered capture group.
+    /// </summary>
+    public class NumberedCapture : GrammarNodeContainer<Char>
+    {
+        /// <summary>
+        /// The capture's position.
+        /// </summary>
+        public Int32 Position { get; }
+
+        /// <summary>
+        /// Initializes a new numbered capture group.
+        /// </summary>
+        /// <param name="position"></param>
+        /// <param name="node"></param>
+        public NumberedCapture ( Int32 position, GrammarNode<Char> node ) : base ( node )
+        {
+            this.Position = position;
+        }
+    }
+}
