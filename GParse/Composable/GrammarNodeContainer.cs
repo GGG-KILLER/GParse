9afc710d6ee21cf34d163712f27b8facf4393488
GGG KILLER <gggkiller2@gmail.com>
2020-09-21 12:52:41 -0300

    Add a base grammar node container class.

---
+++ GParse/Composable/GrammarNodeContainer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GParse.Composable
+{
+    /// <summary>
+    /// The base class for nodes that wrap other nodes.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public abstract class GrammarNodeContainer<T> : GrammarNode<T>
+    {
+        /// <summary>
+        /// The inner node.
+        /// </summary>
+        public GrammarNode<T> InnerNode { get; }
+
+        /// <summary>
+        /// Initializes a new node container with the provided node.
+        /// </summary>
+        /// <param name="node"></param>
+        protected GrammarNodeContainer ( GrammarNode<T> node )
+        {
+            this.InnerNode = node ?? throw new ArgumentNullException ( nameof ( node ) );
+        }
+    }
+}
