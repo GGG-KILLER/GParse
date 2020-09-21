1077a38556e572763de7274cfc8d9e9f0f3c814f
GGG KILLER <gggkiller2@gmail.com>
2020-09-21 12:49:34 -0300

    Do operations in the overloadable methods instead of in the operators.

--- GParse/Composable/GrammarNode.cs
+++ GParse/Composable/GrammarNode.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 
 namespace GParse.Composable
 {
@@ -10,21 +9,21 @@ namespace GParse.Composable
     {
         /// <inheritdoc cref="operator |(GrammarNode{T}, GrammarNode{T})" />
         /// <param name="other">The node to create an alternation with.</param>
-        public Alternation<T> Or ( GrammarNode<T> other ) =>
-            this | other;
+        public virtual Alternation<T> Or ( GrammarNode<T> other ) =>
+            new Alternation<T> ( this, other );
 
         /// <inheritdoc cref="operator &amp;(GrammarNode{T}, GrammarNode{T})" />
         /// <param name="other">The node to create a sequence with.</param>
-        public Sequence<T> And ( GrammarNode<T> other ) =>
-            this & other;
+        public virtual Sequence<T> Then ( GrammarNode<T> other ) =>
+            new Sequence<T> ( this, other );
 
         /// <inheritdoc cref="operator *(GrammarNode{T}, UInt32)" />
-        public Repetition<T> Repeatedly ( UInt32 repetitions ) =>
-            this * repetitions;
+        public virtual Repetition<T> Repeatedly ( UInt32 repetitions ) =>
+            new Repetition<T> ( this, new RepetitionRange ( repetitions, repetitions ) );
 
         /// <inheritdoc cref="operator *(GrammarNode{T}, RepetitionRange)" />
-        public Repetition<T> Repeatedly ( (UInt32? minimum, UInt32? maximum) repetitions ) =>
-            this * repetitions;
+        public virtual Repetition<T> Repeatedly ( RepetitionRange repetitions ) =>
+            new Repetition<T> ( this, repetitions );
 
         /// <summary>
         /// Creates an alternation node.
@@ -34,16 +33,11 @@ namespace GParse.Composable
         /// <returns></returns>
         public static Alternation<T> operator | ( GrammarNode<T> left, GrammarNode<T> right )
         {
-            var nodes = new List<GrammarNode<T>> ( );
-            if ( left is Alternation<T> leftAlternation )
-                nodes.AddRange ( leftAlternation.GrammarNodes );
-            else
-                nodes.Add ( left );
-            if ( right is Alternation<T> rightAlternation )
-                nodes.AddRange ( rightAlternation.GrammarNodes );
-            else
-                nodes.Add ( right );
-            return new Alternation<T> ( nodes.ToArray ( ) );
+            if ( left is null )
+                throw new ArgumentNullException ( nameof ( left ) );
+            if ( right is null )
+                throw new ArgumentNullException ( nameof ( right ) );
+            return left.Or ( right );
         }
 
         /// <summary>
@@ -54,16 +48,11 @@ namespace GParse.Composable
         /// <returns></returns>
         public static Sequence<T> operator & ( GrammarNode<T> left, GrammarNode<T> right )
         {
-            var nodes = new List<GrammarNode<T>> ( );
-            if ( left is Sequence<T> leftAlternation )
-                nodes.AddRange ( leftAlternation.GrammarNodes );
-            else
-                nodes.Add ( left );
-            if ( right is Sequence<T> rightAlternation )
-                nodes.AddRange ( rightAlternation.GrammarNodes );
-            else
-                nodes.Add ( right );
-            return new Sequence<T> ( nodes.ToArray ( ) );
+            if ( left is null )
+                throw new ArgumentNullException ( nameof ( left ) );
+            if ( right is null )
+                throw new ArgumentNullException ( nameof ( right ) );
+            return left.Then ( right );
         }
 
         /// <summary>
@@ -72,8 +61,12 @@ namespace GParse.Composable
         /// <param name="left">The node to be matched.</param>
         /// <param name="repetitions">The number of times the node should be matched.</param>
         /// <returns></returns>
-        public static Repetition<T> operator * ( GrammarNode<T> left, UInt32 repetitions ) =>
-            new Repetition<T> ( left, (repetitions, repetitions) );
+        public static Repetition<T> operator * ( GrammarNode<T> left, UInt32 repetitions )
+        {
+            if ( left is null )
+                throw new ArgumentNullException ( nameof ( left ) );
+            return left.Repeatedly ( repetitions );
+        }
 
         /// <summary>
         /// Creates a repetition node
@@ -81,7 +74,11 @@ namespace GParse.Composable
         /// <param name="left">The node to be matched.</param>
         /// <param name="repetitions">The range of times the node should be matched.</param>
         /// <returns></returns>
-        public static Repetition<T> operator * ( GrammarNode<T> left, RepetitionRange repetitions ) =>
-            new Repetition<T> ( left, repetitions );
+        public static Repetition<T> operator * ( GrammarNode<T> left, RepetitionRange repetitions )
+        {
+            if ( left is null )
+                throw new ArgumentNullException ( nameof ( left ) );
+            return left.Repeatedly ( repetitions );
+        }
     }
 }
