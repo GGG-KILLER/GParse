4c1bf411267cd5f1d3b42439a19ef1d8e7c376ae
GGG KILLER <gggkiller2@gmail.com>
2020-09-26 14:39:51 -0300

    Add rest of benchmarks project.

---
+++ GParse.Benchmarks/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Running;
+
+namespace GParse.Benchmarks
+{
+    class Program
+    {
+        static void Main ( String[] args ) =>
+            BenchmarkSwitcher.FromAssembly ( typeof ( Program ).Assembly )
+                .Run (
+                    args
+#if DEBUG
+                    , new DebugInProcessConfig ( )
+#endif
+                );
+    }
+}
