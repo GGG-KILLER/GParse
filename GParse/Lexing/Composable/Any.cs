892a3a6ac43752b7f2f6d3a71961a257639b54ac
GGG KILLER <gggkiller2@gmail.com>
2020-09-28 15:55:42 -0300

    Add Any and Empty nodes.

---
+++ GParse/Lexing/Composable/Any.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using GParse.Composable;
+
+namespace GParse.Lexing.Composable
+{
+    /// <summary>
+    /// Represents a node that matches any single char.
+    /// </summary>
+    public sealed class Any : GrammarNode<Char>
+    {
+    }
+}
---
+++ GParse/Lexing/Composable/Empty.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using GParse.Composable;
+
+namespace GParse.Lexing.Composable
+{
+    /// <summary>
+    /// Represents a node that will always succeed with a length of 0.
+    /// </summary>
+    public sealed class Empty : GrammarNode<Char>
+    {
+    }
+}
