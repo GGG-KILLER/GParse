00679249c7e5225a55927a6042008f2da1bd9557
GGG KILLER <gggkiller2@gmail.com>
2020-09-27 21:04:06 -0300

    Add regex character set optimization algorithms.

---
+++ GParse.Tests/Lexing/Composable/OptimizationAlgorithmsTests.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using GParse.Lexing.Composable;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GParse.Tests.Lexing.Composable
+{
+    [TestClass]
+    public class OptimizationAlgorithmsTests
+    {
+        [DataTestMethod]
+        [DataRow ( "123456", "1-6" )]
+        [DataRow ( "123", "1-3" )]
+        [DataRow ( "123567", "1-3|5-7" )]
+        [DataRow ( "13456", "1|3-6" )]
+        [DataRow ( "134568", "1|3-6|8" )]
+        [DataRow ( "1235789", "1-3|5|7-9" )]
+        [DataRow ( "abcdefghijklmnopqrstuvwxyz0123456789", "a-z|0-9" )]
+        [DataRow ( "abcdefghijkmopqrstuwxyz023456789", "a-k|m|o-u|w-z|0|2-9" )]
+        [DataRow ( "acegikmoqsuwy02468", "a|c|e|g|i|k|m|o|q|s|u|w|y|0|2|4|6|8" )]
+        public void RangifyCharacters_CreatesRangesProperly ( String characterSetString, String resultString )
+        {
+            // Setup
+            var parts = resultString.Split ( '|' );
+            var expectedCharacters = parts.Where ( s => s.Length == 1 )
+                                          .Select ( s => s[0] )
+                                          .ToArray ( );
+            (Char, Char)[] expectedRanges = parts.Where ( s => s.Length == 3 )
+                                                 .Select ( s => (s[0], s[2]) )
+                                                 .ToArray ( );
+            var characters = characterSetString.ToList ( );
+            var ranges = new List<CharacterRange> ( );
+
+            // Act
+            OptimizationAlgorithms.RangifyCharacters ( characters, ranges );
+
+            // Check
+            (Char, Char)[] outRangesAsTuples = ranges.Select ( range => (range.Start, range.End) ).ToArray ( );
+            CollectionAssert.AreEquivalent ( expectedRanges, outRangesAsTuples, $"Ranges don't match: {String.Join ( ", ", outRangesAsTuples )}" );
+            CollectionAssert.AreEquivalent ( expectedCharacters, characters, $"Characters don't match: {String.Join ( ", ", characters )}" );
+        }
+
+        [DataTestMethod]
+        [DataRow ( "a-d|d-g|g-k", "a-k" )]
+        [DataRow ( "a-d|e-h|i-m", "a-m" )]
+        [DataRow ( "a-e|g-k", "a-e|g-k" )]
+        [DataRow ( "a-d|e-h|j-m|n-t", "a-h|j-t" )]
+        public void MergeRanges_MergesRangesProperly ( String providedRanges, String expectedRanges )
+        {
+            // Setup
+            var ranges = providedRanges.Split ( '|' )
+                                       .Select ( s => new CharacterRange ( s[0], s[2] ) )
+                                       .ToList ( );
+            var expected = expectedRanges.Split ( '|' )
+                                         .Select ( s => (s[0], s[2]) )
+                                         .ToList ( );
+
+            // Act
+            OptimizationAlgorithms.MergeRanges ( ranges );
+
+            // Check
+            (Char, Char)[] tupleRanges = ranges.Select ( range => (range.Start, range.End) ).ToArray ( );
+            CollectionAssert.AreEquivalent ( expected, tupleRanges, $"Ranges don't match: {String.Join ( ", ", tupleRanges )}" );
+        }
+    }
+}
---
+++ GParse/Lexing/Composable/OptimizationAlgorithms.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+
+namespace GParse.Lexing.Composable
+{
+    internal static class OptimizationAlgorithms
+    {
+#if NETCOREAPP3_1 || NET5_0
+        [MethodImpl ( MethodImplOptions.AggressiveOptimization )]
+#endif
+        public static void RangifyCharacters (
+            List<Char> characters,
+            List<CharacterRange> ranges )
+        {
+            // Rangify charset
+            if ( characters.Count <= 2 )
+            {
+                return;
+            }
+
+            characters.Sort ( );
+            Char start = characters[0],
+                end = characters[0];
+            var idx = 1;
+            while ( true )
+            {
+            loopStart:
+                if ( idx >= characters.Count )
+                    break;
+
+                var current = characters[idx];
+
+                if ( end + 1 == current )
+                {
+                    characters.RemoveAt ( idx );
+                    end++;
+                    goto loopStart;
+                }
+                else
+                {
+                    if ( start != end )
+                    {
+                        characters.RemoveAt ( idx - 1 ); // remove the range start.
+                        ranges.Add ( new CharacterRange ( start, end ) );
+                        start = end = current;
+                        goto loopStart;
+                    }
+                    start = end = current;
+                }
+
+                idx++;
+            }
+
+            if ( start != end )
+            {
+                characters.RemoveAt ( idx - 1 ); // remove the range start.
+                ranges.Add ( new CharacterRange ( start, end ) );
+            }
+        }
+
+#if NETCOREAPP3_1 || NET5_0
+        [MethodImpl ( MethodImplOptions.AggressiveOptimization )]
+#endif
+        public static void MergeRanges ( List<CharacterRange> ranges )
+        {
+            for ( var outerIdx = 0; outerIdx < ranges.Count; outerIdx++ )
+            {
+                CharacterRange range1 = ranges[outerIdx];
+                var innerIdx = outerIdx + 1;
+                while ( true )
+                {
+                innerLoopStart:
+                    if ( innerIdx >= ranges.Count )
+                        break;
+                    CharacterRange range2 = ranges[innerIdx];
+
+                    if ( intersectsWith ( range1.Start, range1.End, range2.Start, range2.End )
+                         || System.Math.Abs ( range2.Start - range1.End ) == 1 )
+                    {
+                        ranges.RemoveAt ( innerIdx );
+                        range1 = new CharacterRange ( min ( range1.Start, range2.Start ), max ( range1.End, range2.End ) );
+                        ranges[outerIdx] = range1;
+                        goto innerLoopStart;
+                    }
+
+                    innerIdx++;
+                }
+            }
+
+            [MethodImpl ( MethodImplOptions.AggressiveInlining )]
+            static Boolean intersectsWith ( Char start1, Char end1, Char start2, Char end2 ) =>
+                start1 <= end2 && start2 <= end1;
+            [MethodImpl ( MethodImplOptions.AggressiveInlining )]
+            static Char min ( Char a, Char b ) =>
+                a < b ? a : b;
+            [MethodImpl ( MethodImplOptions.AggressiveInlining )]
+            static Char max ( Char a, Char b ) =>
+                a > b ? a : b;
+        }
+    }
+}
