2785fbc1bcc9256deccbb817fde2bcf81ac2de1e
GGG KILLER <gggkiller2@gmail.com>
2020-10-12 01:53:47 -0300

    Add node utility methods.

---
+++ GParse/Lexing/Composable/NodeUtils.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using GParse.Composable;
+
+namespace GParse.Lexing.Composable
+{
+    /// <summary>
+    /// Utility methods for <see cref="GrammarNode{T}"/>.
+    /// </summary>
+    public static class NodeUtils
+    {
+        /// <summary>
+        /// Checks whether the node is an alternation set element.
+        /// </summary>
+        /// <param name="node"></param>
+        /// <returns></returns>
+        public static Boolean IsAlternationSetElement ( GrammarNode<Char> node ) =>
+            node is CharacterRange
+                 or CharacterSet
+                 or CharacterTerminal
+                 or UnicodeCategoryTerminal;
+
+        /// <summary>
+        /// Checks whether the provided alternation is equivalent to
+        /// a regex alternation set.
+        /// </summary>
+        /// <param name="alternation"></param>
+        /// <returns></returns>
+        public static Boolean IsAlternationSet ( Alternation<Char> alternation ) =>
+            alternation.GrammarNodes.All ( node => IsAlternationSetElement ( node ) );
+    }
+}
