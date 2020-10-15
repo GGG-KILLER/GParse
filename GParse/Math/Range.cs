a0d990568cdc0c819d2d2160f2411226fbd6deac
GGG KILLER <gggkiller2@gmail.com>
2020-10-15 10:42:39 -0300

    Update Range to not use Unsafe.

--- GParse/Math/Range.cs
+++ GParse/Math/Range.cs
@@ -58,85 +58,81 @@
         /// <returns></returns>
         public Boolean IntersectsWith ( Range<T> other )
         {
-            T start = this.Start;
-            T end = this.End;
-            T otherStart = other.Start;
-            T otherEnd = other.End;
             if ( typeof ( T ) == typeof ( SByte ) )
             {
-                var i8Start = Unsafe.As<T, SByte> ( ref start );
-                var i8End = Unsafe.As<T, SByte> ( ref end );
-                var i8OtherStart = Unsafe.As<T, SByte> ( ref otherStart );
-                var i8OtherEnd = Unsafe.As<T, SByte> ( ref otherEnd );
+                var i8Start = ( SByte ) ( Object ) this.Start;
+                var i8End = ( SByte ) ( Object ) this.End;
+                var i8OtherStart = ( SByte ) ( Object ) other.Start;
+                var i8OtherEnd = ( SByte ) ( Object ) other.End;
                 return i8Start <= i8OtherEnd && i8OtherStart <= i8End;
             }
             else if ( typeof ( T ) == typeof ( Byte ) )
             {
-                var u8Start = Unsafe.As<T, Byte> ( ref start );
-                var u8End = Unsafe.As<T, Byte> ( ref end );
-                var u8OtherStart = Unsafe.As<T, Byte> ( ref otherStart );
-                var u8OtherEnd = Unsafe.As<T, Byte> ( ref otherEnd );
+                var u8Start = ( Byte ) ( Object ) this.Start;
+                var u8End = ( Byte ) ( Object ) this.End;
+                var u8OtherStart = ( Byte ) ( Object ) other.Start;
+                var u8OtherEnd = ( Byte ) ( Object ) other.End;
                 return u8Start <= u8OtherEnd && u8OtherStart <= u8End;
             }
             else if ( typeof ( T ) == typeof ( Char ) )
             {
-                var charStart = Unsafe.As<T, Char> ( ref start );
-                var charEnd = Unsafe.As<T, Char> ( ref end );
-                var charOtherStart = Unsafe.As<T, Char> ( ref otherStart );
-                var charOtherEnd = Unsafe.As<T, Char> ( ref otherEnd );
+                var charStart = ( Char ) ( Object ) this.Start;
+                var charEnd = ( Char ) ( Object ) this.End;
+                var charOtherStart = ( Char ) ( Object ) other.Start;
+                var charOtherEnd = ( Char ) ( Object ) other.End;
                 return charStart <= charOtherEnd && charOtherStart <= charEnd;
             }
             else if ( typeof ( T ) == typeof ( Int16 ) )
             {
-                var i16Start = Unsafe.As<T, Int16> ( ref start );
-                var i16End = Unsafe.As<T, Int16> ( ref end );
-                var i16OtherStart = Unsafe.As<T, Int16> ( ref otherStart );
-                var i16OtherEnd = Unsafe.As<T, Int16> ( ref otherEnd );
+                var i16Start = ( Int16 ) ( Object ) this.Start;
+                var i16End = ( Int16 ) ( Object ) this.End;
+                var i16OtherStart = ( Int16 ) ( Object ) other.Start;
+                var i16OtherEnd = ( Int16 ) ( Object ) other.End;
                 return i16Start <= i16OtherEnd && i16OtherStart <= i16End;
             }
             else if ( typeof ( T ) == typeof ( UInt16 ) )
             {
-                var u16Start = Unsafe.As<T, UInt16> ( ref start );
-                var u16End = Unsafe.As<T, UInt16> ( ref end );
-                var u16OtherStart = Unsafe.As<T, UInt16> ( ref otherStart );
-                var u16OtherEnd = Unsafe.As<T, UInt16> ( ref otherEnd );
+                var u16Start = ( UInt16 ) ( Object ) this.Start;
+                var u16End = ( UInt16 ) ( Object ) this.End;
+                var u16OtherStart = ( UInt16 ) ( Object ) other.Start;
+                var u16OtherEnd = ( UInt16 ) ( Object ) other.End;
                 return u16Start <= u16OtherEnd && u16OtherStart <= u16End;
             }
             else if ( typeof ( T ) == typeof ( Int32 ) )
             {
-                var i32Start = Unsafe.As<T, Int32> ( ref start );
-                var i32End = Unsafe.As<T, Int32> ( ref end );
-                var i32OtherStart = Unsafe.As<T, Int32> ( ref otherStart );
-                var i32OtherEnd = Unsafe.As<T, Int32> ( ref otherEnd );
+                var i32Start = ( Int32 ) ( Object ) this.Start;
+                var i32End = ( Int32 ) ( Object ) this.End;
+                var i32OtherStart = ( Int32 ) ( Object ) other.Start;
+                var i32OtherEnd = ( Int32 ) ( Object ) other.End;
                 return i32Start <= i32OtherEnd && i32OtherStart <= i32End;
             }
             else if ( typeof ( T ) == typeof ( UInt32 ) )
             {
-                var u32Start = Unsafe.As<T, UInt32> ( ref start );
-                var u32End = Unsafe.As<T, UInt32> ( ref end );
-                var u32OtherStart = Unsafe.As<T, UInt32> ( ref otherStart );
-                var u32OtherEnd = Unsafe.As<T, UInt32> ( ref otherEnd );
+                var u32Start = ( UInt32 ) ( Object ) this.Start;
+                var u32End = ( UInt32 ) ( Object ) this.End;
+                var u32OtherStart = ( UInt32 ) ( Object ) other.Start;
+                var u32OtherEnd = ( UInt32 ) ( Object ) other.End;
                 return u32Start <= u32OtherEnd && u32OtherStart <= u32End;
             }
             else if ( typeof ( T ) == typeof ( Int64 ) )
             {
-                var i64Start = Unsafe.As<T, Int64> ( ref start );
-                var i64End = Unsafe.As<T, Int64> ( ref end );
-                var i64OtherStart = Unsafe.As<T, Int64> ( ref otherStart );
-                var i64OtherEnd = Unsafe.As<T, Int64> ( ref otherEnd );
+                var i64Start = ( Int64 ) ( Object ) this.Start;
+                var i64End = ( Int64 ) ( Object ) this.End;
+                var i64OtherStart = ( Int64 ) ( Object ) other.Start;
+                var i64OtherEnd = ( Int64 ) ( Object ) other.End;
                 return i64Start <= i64OtherEnd && i64OtherStart <= i64End;
             }
             else if ( typeof ( T ) == typeof ( UInt64 ) )
             {
-                var u64Start = Unsafe.As<T, UInt64> ( ref start );
-                var u64End = Unsafe.As<T, UInt64> ( ref end );
-                var u64OtherStart = Unsafe.As<T, UInt64> ( ref otherStart );
-                var u64OtherEnd = Unsafe.As<T, UInt64> ( ref otherEnd );
+                var u64Start = ( UInt64 ) ( Object ) this.Start;
+                var u64End = ( UInt64 ) ( Object ) this.End;
+                var u64OtherStart = ( UInt64 ) ( Object ) other.Start;
+                var u64OtherEnd = ( UInt64 ) ( Object ) other.End;
                 return u64Start <= u64OtherEnd && u64OtherStart <= u64End;
             }
             else
             {
-                return start.CompareTo ( otherEnd ) < 1 && otherStart.CompareTo ( end ) < 1;
+                return this.Start.CompareTo ( other.End ) < 1 && other.Start.CompareTo ( this.End ) < 1;
             }
         }
 
@@ -157,39 +153,37 @@
         [MethodImpl ( MethodImplOptions.AggressiveInlining )]
         public Boolean ValueIn ( T value )
         {
-            T start = this.Start;
-            T end = this.End;
             if ( typeof ( T ) == typeof ( Byte ) )
             {
-                var u8Value = Unsafe.As<T, Byte> ( ref value );
-                var u8Start = Unsafe.As<T, Byte> ( ref start );
-                var u8End = Unsafe.As<T, Byte> ( ref end );
+                var u8Value = ( Byte ) ( Object ) value;
+                var u8Start = ( Byte ) ( Object ) this.Start;
+                var u8End = ( Byte ) ( Object ) this.End;
                 return ( UInt16 ) ( u8Value - u8Start ) <= ( u8End - u8Start );
             }
             else if ( typeof ( T ) == typeof ( UInt16 ) )
             {
-                var u16Value = Unsafe.As<T, UInt16> ( ref value );
-                var u16Start = Unsafe.As<T, UInt16> ( ref start );
-                var u16End = Unsafe.As<T, UInt16> ( ref end );
+                var u16Value = ( UInt16 ) ( Object ) value;
+                var u16Start = ( UInt16 ) ( Object ) this.Start;
+                var u16End = ( UInt16 ) ( Object ) this.End;
                 return ( UInt32 ) ( u16Value - u16Start ) <= ( u16End - u16Start );
             }
             else if ( typeof ( T ) == typeof ( Char ) )
             {
-                var charValue = Unsafe.As<T, Char> ( ref value );
-                var charStart = Unsafe.As<T, Char> ( ref start );
-                var charEnd = Unsafe.As<T, Char> ( ref end );
+                var charValue = ( Char ) ( Object ) value;
+                var charStart = ( Char ) ( Object ) this.Start;
+                var charEnd = ( Char ) ( Object ) this.End;
                 return ( UInt32 ) ( charValue - charStart ) <= ( charEnd - charStart );
             }
             else if ( typeof ( T ) == typeof ( UInt32 ) )
             {
-                var u32Value = Unsafe.As<T, UInt32> ( ref value );
-                var u32Start = Unsafe.As<T, UInt32> ( ref start );
-                var u32End = Unsafe.As<T, UInt32> ( ref end );
+                var u32Value = ( UInt32 ) ( Object ) value;
+                var u32Start = ( UInt32 ) ( Object ) this.Start;
+                var u32End = ( UInt32 ) ( Object ) this.End;
                 return ( UInt64 ) ( u32Value - u32Start ) <= ( u32End - u32Start );
             }
             else
             {
-                return start.CompareTo ( value ) < 1 && value.CompareTo ( end ) < 1;
+                return this.Start.CompareTo ( value ) < 1 && value.CompareTo ( this.End ) < 1;
             }
         }
 
@@ -258,56 +252,56 @@
         {
             if ( typeof ( T ) == typeof ( SByte ) )
             {
-                var i8x = Unsafe.As<T, SByte> ( ref x );
-                var i8y = Unsafe.As<T, SByte> ( ref y );
+                var i8x = ( SByte ) ( Object ) x;
+                var i8y = ( SByte ) ( Object ) y;
                 return i8x <= i8y ? x : y;
             }
             else if ( typeof ( T ) == typeof ( Byte ) )
             {
-                var u8x = Unsafe.As<T, Byte> ( ref x );
-                var u8y = Unsafe.As<T, Byte> ( ref y );
+                var u8x = ( Byte ) ( Object ) x;
+                var u8y = ( Byte ) ( Object ) y;
                 return u8x <= u8y ? x : y;
             }
             else if ( typeof ( T ) == typeof ( Char ) )
             {
-                var charx = Unsafe.As<T, Char> ( ref x );
-                var chary = Unsafe.As<T, Char> ( ref y );
+                var charx = ( Char ) ( Object ) x;
+                var chary = ( Char ) ( Object ) y;
                 return charx <= chary ? x : y;
             }
             else if ( typeof ( T ) == typeof ( Int16 ) )
             {
-                var i16x = Unsafe.As<T, Int16> ( ref x );
-                var i16y = Unsafe.As<T, Int16> ( ref y );
+                var i16x = ( Int16 ) ( Object ) x;
+                var i16y = ( Int16 ) ( Object ) y;
                 return i16x <= i16y ? x : y;
             }
             else if ( typeof ( T ) == typeof ( UInt16 ) )
             {
-                var u16x = Unsafe.As<T, UInt16> ( ref x );
-                var u16y = Unsafe.As<T, UInt16> ( ref y );
+                var u16x = ( UInt16 ) ( Object ) x;
+                var u16y = ( UInt16 ) ( Object ) y;
                 return u16x <= u16y ? x : y;
             }
             else if ( typeof ( T ) == typeof ( Int32 ) )
             {
-                var i32x = Unsafe.As<T, Int32> ( ref x );
-                var i32y = Unsafe.As<T, Int32> ( ref y );
+                var i32x = ( Int32 ) ( Object ) x;
+                var i32y = ( Int32 ) ( Object ) y;
                 return i32x <= i32y ? x : y;
             }
             else if ( typeof ( T ) == typeof ( UInt32 ) )
             {
-                var u32x = Unsafe.As<T, UInt32> ( ref x );
-                var u32y = Unsafe.As<T, UInt32> ( ref y );
+                var u32x = ( UInt32 ) ( Object ) x;
+                var u32y = ( UInt32 ) ( Object ) y;
                 return u32x <= u32y ? x : y;
             }
             else if ( typeof ( T ) == typeof ( Int64 ) )
             {
-                var i64x = Unsafe.As<T, Int64> ( ref x );
-                var i64y = Unsafe.As<T, Int64> ( ref y );
+                var i64x = ( Int64 ) ( Object ) x;
+                var i64y = ( Int64 ) ( Object ) y;
                 return i64x <= i64y ? x : y;
             }
             else if ( typeof ( T ) == typeof ( UInt64 ) )
             {
-                var u64x = Unsafe.As<T, UInt64> ( ref x );
-                var u64y = Unsafe.As<T, UInt64> ( ref y );
+                var u64x = ( UInt64 ) ( Object ) x;
+                var u64y = ( UInt64 ) ( Object ) y;
                 return u64x <= u64y ? x : y;
             }
             else
@@ -321,56 +315,56 @@
         {
             if ( typeof ( T ) == typeof ( SByte ) )
             {
-                var i8x = Unsafe.As<T, SByte> ( ref x );
-                var i8y = Unsafe.As<T, SByte> ( ref y );
+                var i8x = ( SByte ) ( Object ) x;
+                var i8y = ( SByte ) ( Object ) y;
                 return i8x >= i8y ? x : y;
             }
             else if ( typeof ( T ) == typeof ( Byte ) )
             {
-                var u8x = Unsafe.As<T, Byte> ( ref x );
-                var u8y = Unsafe.As<T, Byte> ( ref y );
+                var u8x = ( Byte ) ( Object ) x;
+                var u8y = ( Byte ) ( Object ) y;
                 return u8x >= u8y ? x : y;
             }
             else if ( typeof ( T ) == typeof ( Char ) )
             {
-                var charx = Unsafe.As<T, Char> ( ref x );
-                var chary = Unsafe.As<T, Char> ( ref y );
+                var charx = ( Char ) ( Object ) x;
+                var chary = ( Char ) ( Object ) y;
                 return charx >= chary ? x : y;
             }
             else if ( typeof ( T ) == typeof ( Int16 ) )
             {
-                var i16x = Unsafe.As<T, Int16> ( ref x );
-                var i16y = Unsafe.As<T, Int16> ( ref y );
+                var i16x = ( Int16 ) ( Object ) x;
+                var i16y = ( Int16 ) ( Object ) y;
                 return i16x >= i16y ? x : y;
             }
             else if ( typeof ( T ) == typeof ( UInt16 ) )
             {
-                var u16x = Unsafe.As<T, UInt16> ( ref x );
-                var u16y = Unsafe.As<T, UInt16> ( ref y );
+                var u16x = ( UInt16 ) ( Object ) x;
+                var u16y = ( UInt16 ) ( Object ) y;
                 return u16x >= u16y ? x : y;
             }
             else if ( typeof ( T ) == typeof ( Int32 ) )
             {
-                var i32x = Unsafe.As<T, Int32> ( ref x );
-                var i32y = Unsafe.As<T, Int32> ( ref y );
+                var i32x = ( Int32 ) ( Object ) x;
+                var i32y = ( Int32 ) ( Object ) y;
                 return i32x >= i32y ? x : y;
             }
             else if ( typeof ( T ) == typeof ( UInt32 ) )
             {
-                var u32x = Unsafe.As<T, UInt32> ( ref x );
-                var u32y = Unsafe.As<T, UInt32> ( ref y );
+                var u32x = ( UInt32 ) ( Object ) x;
+                var u32y = ( UInt32 ) ( Object ) y;
                 return u32x >= u32y ? x : y;
             }
             else if ( typeof ( T ) == typeof ( Int64 ) )
             {
-                var i64x = Unsafe.As<T, Int64> ( ref x );
-                var i64y = Unsafe.As<T, Int64> ( ref y );
+                var i64x = ( Int64 ) ( Object ) x;
+                var i64y = ( Int64 ) ( Object ) y;
                 return i64x >= i64y ? x : y;
             }
             else if ( typeof ( T ) == typeof ( UInt64 ) )
             {
-                var u64x = Unsafe.As<T, UInt64> ( ref x );
-                var u64y = Unsafe.As<T, UInt64> ( ref y );
+                var u64x = ( UInt64 ) ( Object ) x;
+                var u64y = ( UInt64 ) ( Object ) y;
                 return u64x >= u64y ? x : y;
             }
             else
