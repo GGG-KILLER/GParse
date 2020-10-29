9d343ea4b3a1c9685c65903b86b29ab47cb9ca18
GGG KILLER <gggkiller2@gmail.com>
2020-10-29 13:12:10 -0300

    Add CharUtils tests.

---
+++ GParse.Tests/Utilities/CharUtilsTests.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using GParse.Math;
+using GParse.Utilities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GParse.Tests.Utilities
+{
+    [TestClass]
+    public class CharUtilsTests
+    {
+        [TestMethod]
+        public void FlattenRanges_WorksProperly ( )
+        {
+            // Setup
+            var rangeLists = new Range<Char>[][]
+            {
+                Array.Empty<Range<Char>> ( ),
+                new Range<Char>[]
+                {
+                    new ( '0', '9' ),
+                },
+                new Range<Char>[]
+                {
+                    new ( '0', '9' ),
+                    new ( 'A', 'Z' ),
+                },
+                new Range<Char>[]
+                {
+                    new ( '0', '9' ),
+                    new ( 'A', 'Z' ),
+                    new ( 'a', 'z' ),
+                }
+            };
+            var expectedRangeArrays = new List<ImmutableArray<Char>>
+            {
+                ImmutableArray.Create<Char> ( ),
+                ImmutableArray.Create ( '0', '9' ),
+                ImmutableArray.Create ( '0', '9', 'A', 'Z' ),
+                ImmutableArray.Create ( '0', '9', 'A', 'Z', 'a', 'z' ),
+            };
+
+            // Act
+            var obtainedRangeArrays = rangeLists.Select ( CharUtils.FlattenRanges ).ToList ( );
+
+            // Check
+            for ( var arrayIdx = 0; arrayIdx < expectedRangeArrays.Count; arrayIdx++ )
+            {
+                CollectionAssert.AreEqual ( expectedRangeArrays[arrayIdx], obtainedRangeArrays[arrayIdx] );
+            }
+        }
+    }
+}
