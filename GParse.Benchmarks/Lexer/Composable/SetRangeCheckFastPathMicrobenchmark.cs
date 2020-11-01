15aa80a155eaa63a21ff289daf81dbb824ba3fc4
GGG KILLER <gggkiller2@gmail.com>
2020-10-31 23:59:11 -0300

    Fast path for single range benchmark.

---
+++ GParse.Benchmarks/Lexer/Composable/SetRangeCheckFastPathMicrobenchmark.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Immutable;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Jobs;
+using GParse.Math;
+using GParse.Utilities;
+
+namespace GParse.Benchmarks.Lexer.Composable
+{
+    [DisassemblyDiagnoser, MemoryDiagnoser]
+    [SimpleJob ( RuntimeMoniker.Net461 )]
+    [SimpleJob ( RuntimeMoniker.Net48 )]
+    [SimpleJob ( RuntimeMoniker.NetCoreApp21 )]
+    [SimpleJob ( RuntimeMoniker.NetCoreApp31 )]
+    [SimpleJob ( RuntimeMoniker.NetCoreApp50 )]
+    public class SetRangeCheckFastPathMicrobenchmark
+    {
+        public ImmutableArray<Char> Ranges => CharUtils.FlattenRanges ( new[] { new Range<Char> ( 'a', 'z' ) } );
+
+        [Params ( 'g', 'a', '9' )]
+        public Char Value { get; set; }
+
+        [Benchmark ( Baseline = true )]
+        public Boolean BinarySearch ( ) =>
+            CharUtils.IsInRanges ( this.Ranges, this.Value );
+
+        [Benchmark]
+        public Boolean Raw ( )
+        {
+            ImmutableArray<Char> ranges = this.Ranges;
+            return CharUtils.IsInRange ( ranges[0], this.Value, ranges[1] );
+        }
+    }
+}
