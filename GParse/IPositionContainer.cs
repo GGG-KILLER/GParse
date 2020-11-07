798fb7e5e76e1bbcd1e51d59cb6dd513985f0ac0
GGG KILLER <gggkiller2@gmail.com>
2020-11-07 17:12:42 -0300

    Add interfaces for position containers.

---
+++ GParse/IPositionContainer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using GParse.Math;
+
+namespace GParse
+{
+    /// <summary>
+    /// The interface of classes that act over code.
+    /// </summary>
+    public interface IPositionContainer
+    {
+        /// <summary>
+        /// The code's length.
+        /// </summary>
+        Int32 Length { get; }
+
+        /// <summary>
+        /// The current position.
+        /// </summary>
+        Int32 Position { get; }
+
+        /// <summary>
+        /// The <see cref="SourceLocation"/> of the current position.
+        /// </summary>
+        SourceLocation GetLocation ( );
+
+        /// <summary>
+        /// Obtains the <see cref="SourceLocation"/> of a provided <paramref name="position"/>.
+        /// </summary>
+        /// <param name="position">The position to obtain the location of.</param>
+        /// <returns>The obtained location.</returns>
+        SourceLocation GetLocation ( Int32 position );
+
+        /// <summary>
+        /// Translates a <see cref="Range{T}"/> into a <see cref="SourceRange"/>.
+        /// </summary>
+        /// <param name="range">The position range to translate into a source range.</param>
+        /// <returns>The obtained range.</returns>
+        SourceLocation GetLocation ( Range<Int32> range );
+    }
+}
---
+++ GParse/IRestorablePositionContainer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace GParse
+{
+    /// <summary>
+    /// The interface of classes that act over code and can have their positions restored.
+    /// </summary>
+    public interface IRestorablePositionContainer : IPositionContainer
+    {
+        /// <summary>
+        /// Restores the position to a provided <paramref name="location"/>.
+        /// </summary>
+        /// <param name="location">The location the position should be restored to.</param>
+        /// <remarks>
+        /// No validation is done to check that the provided line and column numbers are correct.
+        /// </remarks>
+        void Restore ( SourceLocation location );
+
+        /// <summary>
+        /// Restores the position to a given <paramref name="position"/>.
+        /// </summary>
+        /// <param name="position">The position to be restored to.</param>
+        /// <remarks>
+        /// When restoring a position instead of a <see cref="SourceLocation"/>, if the last
+        /// known location is after the position being restored to, we will have to recalculate
+        /// the position from the start of the code until the provided position to obtain the
+        /// <see cref="SourceLocation"/> when /// <see cref="IPositionContainer.GetLocation()"/>
+        /// is called.
+        /// </remarks>
+        void Restore ( Int32 position );
+    }
+}
