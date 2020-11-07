834ca6d726607d209e47349eec340efb7960715b
GGG KILLER <gggkiller2@gmail.com>
2020-11-06 23:11:43 -0300

    Add range calculation methods.

--- GParse/SourceRange.cs
+++ GParse/SourceRange.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using GParse.Math;
 
 namespace GParse
 {
@@ -12,6 +13,38 @@ namespace GParse
         /// Zero
         /// </summary>
         public static readonly SourceRange Zero = new SourceRange ( SourceLocation.Zero, SourceLocation.Zero );
+
+        /// <summary>
+        /// Translates a position <see cref="Range{T}"/> to a <see cref="SourceRange"/>.
+        /// </summary>
+        /// <param name="input">The string to calculate the locations on.</param>
+        /// <param name="range">The (absolute) position range to calculate the locations of.</param>
+        /// <param name="reference">A location located earlier than the range to use as a reference.</param>
+        /// <returns>The calculated location range.</returns>
+        /// <exception cref="ArgumentOutOfRangeException">
+        /// Thrown when any of the <paramref name="range"/>'s positions are less than zero or greater
+        /// or equal to the <paramref name="input"/> length.
+        /// </exception>
+        /// <exception cref="ArgumentException">
+        /// Thrown when the <paramref name="reference"/> location is located before any of the
+        /// <paramref name="range"/>'s positions.
+        /// </exception>
+        public static SourceRange Calculate ( String input, Range<Int32> range, SourceLocation reference )
+        {
+            var startPosition = SourceLocation.Calculate ( input, range.Start, reference );
+            var endPosition = SourceLocation.Calculate ( input, range.End, startPosition );
+            return new SourceRange ( startPosition, endPosition );
+        }
+
+        /// <summary>
+        /// <inheritdoc cref="Calculate(String, Range{Int32}, SourceLocation)"/>
+        /// </summary>
+        /// <param name="input"><inheritdoc cref="Calculate(String, Range{Int32}, SourceLocation)"/></param>
+        /// <param name="range"><inheritdoc cref="Calculate(String, Range{Int32}, SourceLocation)"/></param>
+        /// <returns><inheritdoc cref="Calculate(String, Range{Int32}, SourceLocation)"/></returns>
+        /// <exception cref="ArgumentOutOfRangeException"><inheritdoc cref="Calculate(String, Range{Int32}, SourceLocation)"/></exception>
+        public static SourceRange Calculate ( String input, Range<Int32> range ) =>
+            Calculate ( input, range, SourceLocation.Zero );
 
         /// <summary>
         /// Starting location
