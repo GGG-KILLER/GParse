60c53d5701132e57f39335c11c727d67d5dd3fc8
GGG KILLER <gggkiller2@gmail.com>
2020-11-08 14:49:15 -0300

    Fix ReadToEnd methods and bump version.

--- GParse/IO/StringCodeReader.cs
+++ GParse/IO/StringCodeReader.cs
@@ -617,7 +617,7 @@ namespace GParse.IO
         public String ReadToEnd ( )
         {
             var ret = this._code.Substring ( this.Position );
-            this.Position = this.Length;
+            this.Advance ( this.Length - this.Position );
             return ret;
         }
 
@@ -743,7 +743,7 @@ namespace GParse.IO
         public ReadOnlySpan<Char> ReadSpanToEnd ( )
         {
             ReadOnlySpan<Char> ret = this._code.AsSpan ( this.Position );
-            this.Position = this.Length;
+            this.Advance ( this.Length - this.Position );
             return ret;
         }
 
