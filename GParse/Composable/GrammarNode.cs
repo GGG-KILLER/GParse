577e4b74ec996f5e7cc0c18693656826b4bf1a30
GGG KILLER <gggkiller2@gmail.com>
2020-11-13 15:13:33 -0300

    Update composition methods to not use keywords as names and add suppressions.

--- GParse/Composable/GrammarNode.cs
+++ GParse/Composable/GrammarNode.cs
@@ -10,21 +10,21 @@ namespace GParse.Composable
     {
         /// <inheritdoc cref="operator |(GrammarNode{T}, GrammarNode{T})" />
         /// <param name="other">The node to create an alternation with.</param>
-        public virtual Alternation<T> Or ( GrammarNode<T> other ) =>
-            new Alternation<T> ( this, other );
+        public virtual Alternation<T> OrThen ( GrammarNode<T> other ) =>
+            new ( this, other );
 
         /// <inheritdoc cref="operator &amp;(GrammarNode{T}, GrammarNode{T})" />
         /// <param name="other">The node to create a sequence with.</param>
-        public virtual Sequence<T> Then ( GrammarNode<T> other ) =>
-            new Sequence<T> ( this, other );
+        public virtual Sequence<T> AndThen ( GrammarNode<T> other ) =>
+            new ( this, other );
 
         /// <inheritdoc cref="operator *(GrammarNode{T}, UInt32)" />
         public virtual Repetition<T> Repeatedly ( UInt32 repetitions ) =>
-            new Repetition<T> ( this, new RepetitionRange ( repetitions, repetitions ), false );
+            new ( this, new RepetitionRange ( repetitions, repetitions ), false );
 
         /// <inheritdoc cref="operator *(GrammarNode{T}, RepetitionRange)" />
         public virtual Repetition<T> Repeatedly ( RepetitionRange repetitions ) =>
-            new Repetition<T> ( this, repetitions, false );
+            new ( this, repetitions, false );
 
         /// <summary>
         /// Creates an alternation node.
@@ -32,13 +32,14 @@ namespace GParse.Composable
         /// <param name="left"></param>
         /// <param name="right"></param>
         /// <returns></returns>
+        [SuppressMessage ( "Usage", "CA2225:Operator overloads have named alternates", Justification = "Has OrThen method." )]
         public static Alternation<T> operator | ( GrammarNode<T> left, GrammarNode<T> right )
         {
             if ( left is null )
                 throw new ArgumentNullException ( nameof ( left ) );
             if ( right is null )
                 throw new ArgumentNullException ( nameof ( right ) );
-            return left.Or ( right );
+            return left.OrThen ( right );
         }
 
         /// <summary>
@@ -47,13 +48,14 @@ namespace GParse.Composable
         /// <param name="left"></param>
         /// <param name="right"></param>
         /// <returns></returns>
+        [SuppressMessage ( "Usage", "CA2225:Operator overloads have named alternates", Justification = "Has AndThen method." )]
         public static Sequence<T> operator & ( GrammarNode<T> left, GrammarNode<T> right )
         {
             if ( left is null )
                 throw new ArgumentNullException ( nameof ( left ) );
             if ( right is null )
                 throw new ArgumentNullException ( nameof ( right ) );
-            return left.Then ( right );
+            return left.AndThen ( right );
         }
 
         /// <summary>
