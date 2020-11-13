f2d4706b8d0fbececce5e117700e4805925b3799
GGG KILLER <gggkiller2@gmail.com>
2020-11-13 16:32:21 -0300

    Add suppressions and null handling.

--- GParse/Lexing/Composable/CharacterTerminal.cs
+++ GParse/Lexing/Composable/CharacterTerminal.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics.CodeAnalysis;
 using GParse.Composable;
 using GParse.Utilities;
 
@@ -17,20 +18,24 @@ namespace GParse.Lexing.Composable
         {
         }
 
+
         /// <summary>
         /// Converts a char to a char terminal.
         /// </summary>
         /// <param name="ch"></param>
+        [SuppressMessage ( "Usage", "CA2225:Operator overloads have named alternates", Justification = "The constructor can be used instead." )]
         public static implicit operator CharacterTerminal ( Char ch ) =>
             new CharacterTerminal ( ch );
+
 
         /// <summary>
         /// Negates this char terminal to match nodes that are not this char.
         /// </summary>
         /// <param name="charTerminal"></param>
         /// <returns></returns>
-        public static NegatedCharacterTerminal operator ! ( CharacterTerminal charTerminal ) =>
-            new NegatedCharacterTerminal ( charTerminal.Value );
+        [SuppressMessage ( "Usage", "CA2225:Operator overloads have named alternates", Justification = "There's the Negate extension method." )]
+        public static NegatedCharacterTerminal? operator ! ( CharacterTerminal? charTerminal ) =>
+            charTerminal is null ? null : new NegatedCharacterTerminal ( charTerminal.Value );
 
         /// <summary>
         /// Converts this terminal into a readable character string.
