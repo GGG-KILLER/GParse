b7c24d7aed9538e8924ae610ce82c9f30e587007
GGG KILLER <gggkiller2@gmail.com>
2020-11-13 15:14:13 -0300

    Implement equality and hashing for the Any node.

--- GParse/Lexing/Composable/Any.cs
+++ GParse/Lexing/Composable/Any.cs
@@ -6,8 +6,17 @@ namespace GParse.Lexing.Composable
     /// <summary>
     /// Represents a node that matches any single char.
     /// </summary>
-    public sealed class Any : GrammarNode<Char>
+    public sealed class Any : GrammarNode<Char>, IEquatable<Any>
     {
-        internal static readonly Any Instance = new Any ( );
+        internal static readonly Any Instance = new ( );
+
+        /// <inheritdoc/>
+        public Boolean Equals ( Any? other ) => other is not null;
+
+        /// <inheritdoc/>
+        public override Boolean Equals ( Object? obj ) => obj is Any;
+
+        /// <inheritdoc/>
+        public override Int32 GetHashCode ( ) => 0;
     }
 }
