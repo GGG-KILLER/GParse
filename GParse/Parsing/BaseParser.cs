d9bc76e5575133effba57503738bca0a802e55a5
GGG KILLER <gggkiller2@gmail.com>
2020-11-14 00:58:26 -0300

    Create a base class for parsers.
    It doesn't do much for now but as I start to use this more I'll probably end up adding some commonly used stuff here.

---
+++ GParse/Parsing/BaseParser.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using GParse.Lexing;
+
+namespace GParse.Parsing
+{
+    /// <summary>
+    /// The base class for parsers.
+    /// </summary>
+    /// <typeparam name="TokenTypeT"></typeparam>
+    public abstract class BaseParser<TokenTypeT>
+        where TokenTypeT : notnull
+    {
+        /// <summary>
+        /// This is the <see cref="DiagnosticList"/> for this parser.
+        /// </summary>
+        protected DiagnosticList Diagnostics { get; }
+
+        /// <inheritdoc />
+        protected ITokenReader<TokenTypeT> TokenReader { get; }
+
+        /// <summary>
+        /// Initializes a new base parser.
+        /// </summary>
+        /// <param name="diagnostics"></param>
+        /// <param name="tokenReader"></param>
+        protected BaseParser ( DiagnosticList diagnostics, ITokenReader<TokenTypeT> tokenReader )
+        {
+            this.Diagnostics = diagnostics ?? throw new ArgumentNullException ( nameof ( diagnostics ) );
+            this.TokenReader = tokenReader ?? throw new ArgumentNullException ( nameof ( tokenReader ) );
+        }
+    }
+}
