f53e0bf5dcfd0d57329b8e3f23fea74a90099da1
GGG KILLER <gggkiller2@gmail.com>
2020-11-13 21:14:11 -0300

    Implement serialization support, improve docs and add suppression.

--- GParse/Lexing/Composable/RegexParseException.cs
+++ GParse/Lexing/Composable/RegexParseException.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.Serialization;
 using GParse.Math;
 
 namespace GParse.Lexing.Composable
@@ -6,6 +8,8 @@ namespace GParse.Lexing.Composable
     /// <summary>
     /// An exception raised while parsing a regex expression.
     /// </summary>
+    [SuppressMessage ( "Design", "CA1032:Implement standard exception constructors", Justification = "Class shouldn't be constructed without a range." )]
+    [Serializable]
     public sealed class RegexParseException : Exception
     {
         /// <summary>
@@ -16,8 +20,8 @@ namespace GParse.Lexing.Composable
         /// <summary>
         /// Initializes a new regex parse exception.
         /// </summary>
-        /// <param name="location"><inheritdoc/></param>
-        /// <param name="message"><inheritdoc/></param>
+        /// <param name="location">The location where the error happened.</param>
+        /// <param name="message">The error message.</param>
         public RegexParseException ( Int32 location, String message ) : base ( message )
         {
             this.Range = new Range<Int32> ( location, location + 1 );
@@ -26,8 +30,8 @@ namespace GParse.Lexing.Composable
         /// <summary>
         /// Initializes a new regex parse exception.
         /// </summary>
-        /// <param name="range"><inheritdoc/></param>
-        /// <param name="message"><inheritdoc/></param>
+        /// <param name="range">The range where the error happened.</param>
+        /// <param name="message"><inheritdoc cref="RegexParseException(Int32, String)"/></param>
         public RegexParseException ( Range<Int32> range, String message ) : base ( message )
         {
             this.Range = range;
@@ -36,9 +40,9 @@ namespace GParse.Lexing.Composable
         /// <summary>
         /// Initializes a new regex parse exception.
         /// </summary>
-        /// <param name="location"><inheritdoc/></param>
-        /// <param name="message"><inheritdoc/></param>
-        /// <param name="innerException"><inheritdoc/></param>
+        /// <param name="location"><inheritdoc cref="RegexParseException(Int32, String)"/></param>
+        /// <param name="message"><inheritdoc cref="RegexParseException(Int32, String)"/></param>
+        /// <param name="innerException"><inheritdoc cref="Exception(String, Exception)"/></param>
         public RegexParseException ( Int32 location, String message, Exception innerException ) : base ( message, innerException )
         {
             this.Range = new Range<Int32> ( location );
@@ -47,12 +51,30 @@ namespace GParse.Lexing.Composable
         /// <summary>
         /// Initializes a new regex parse exception.
         /// </summary>
-        /// <param name="range"><inheritdoc/></param>
-        /// <param name="message"><inheritdoc/></param>
-        /// <param name="innerException"></param>
+        /// <param name="range"><inheritdoc cref="RegexParseException(Range{Int32}, String)"/></param>
+        /// <param name="message"><inheritdoc cref="RegexParseException(Range{Int32}, String)"/></param>
+        /// <param name="innerException"><inheritdoc cref="Exception(String, Exception)"/></param>
         public RegexParseException ( Range<Int32> range, String message, Exception innerException ) : base ( message, innerException )
         {
             this.Range = range;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RegexParseException"/> class with serialized data.
+        /// </summary>
+        /// <param name="serializationInfo"><inheritdoc cref="RegexParseException(SerializationInfo, StreamingContext)"/></param>
+        /// <param name="streamingContext"><inheritdoc cref="RegexParseException(SerializationInfo, StreamingContext)"/></param>
+        private RegexParseException ( SerializationInfo serializationInfo, StreamingContext streamingContext )
+            : base ( serializationInfo, streamingContext )
+        {
+            this.Range = ( Range<Int32> ) serializationInfo.GetValue ( "RegexParseException.Range", typeof ( Range<Int32> ) );
+        }
+
+        /// <inheritdoc/>
+        public override void GetObjectData ( SerializationInfo info, StreamingContext context )
+        {
+            base.GetObjectData ( info, context );
+            info.AddValue ( "RegexParseException.Range", this.Range, typeof ( Range<Int32> ) );
         }
     }
 }
