49b7a1cecdffc37613387adadb964fedffbfaa98
GGG KILLER <gggkiller2@gmail.com>
2020-11-13 21:05:52 -0300

    Update negation to support null and add suppressions.

--- GParse/Lexing/Composable/NegativeLookahead.cs
+++ GParse/Lexing/Composable/NegativeLookahead.cs
@@ -1,6 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using System.Diagnostics.CodeAnalysis;
 using GParse.Composable;
 
 namespace GParse.Lexing.Composable
@@ -18,13 +17,16 @@ namespace GParse.Lexing.Composable
         {
         }
 
+
         /// <summary>
         /// Negates this lookahead.
         /// </summary>
         /// <param name="negativeLookahead"></param>
         /// <returns></returns>
-        public static Lookahead operator ! ( NegativeLookahead negativeLookahead ) =>
-            new Lookahead ( negativeLookahead.InnerNode );
+        [SuppressMessage ( "Usage", "CA2225:Operator overloads have named alternates", Justification = "<Pending>" )]
+        [return: NotNullIfNotNull ( "negativeLookahead" )]
+        public static Lookahead? operator ! ( NegativeLookahead? negativeLookahead ) =>
+            negativeLookahead is null ? null : new Lookahead ( negativeLookahead.InnerNode );
 
         /// <summary>
         /// Converts this node back into a regex string.
