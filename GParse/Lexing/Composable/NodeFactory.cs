1f0f41777ed97bff2279bfe1d7d7b65bf6f77921
GGG KILLER <gggkiller2@gmail.com>
2020-11-17 15:51:50 -0300

    Use short new forms.

--- GParse/Lexing/Composable/NodeFactory.cs
+++ GParse/Lexing/Composable/NodeFactory.cs
@@ -13,32 +13,28 @@ namespace GParse.Lexing.Composable
         /// Creates a new <see cref="Any"/>.
         /// </summary>
         /// <returns></returns>
-        public static Any Any ( ) =>
-            new Any ( );
+        public static Any Any ( ) => new ( );
 
         /// <summary>
         /// Creates a new <see cref="CharacterTerminal"/>.
         /// </summary>
         /// <param name="ch"></param>
         /// <returns></returns>
-        public static CharacterTerminal Terminal ( Char ch ) =>
-            new CharacterTerminal ( ch );
+        public static CharacterTerminal Terminal ( Char ch ) => new ( ch );
 
         /// <summary>
         /// Creates a new <see cref="StringTerminal"/>.
         /// </summary>
         /// <param name="str"></param>
         /// <returns></returns>
-        public static StringTerminal Terminal ( String str ) =>
-            new StringTerminal ( str );
+        public static StringTerminal Terminal ( String str ) => new ( str );
 
         /// <summary>
         /// Creates a new <see cref="UnicodeCategoryTerminal"/>.
         /// </summary>
         /// <param name="unicodeCategory"></param>
         /// <returns></returns>
-        public static UnicodeCategoryTerminal Terminal ( UnicodeCategory unicodeCategory ) =>
-            new UnicodeCategoryTerminal ( unicodeCategory );
+        public static UnicodeCategoryTerminal Terminal ( UnicodeCategory unicodeCategory ) => new ( unicodeCategory );
 
         /// <summary>
         /// Creates a new <see cref="CharacterRange"/>.
@@ -46,32 +42,28 @@ namespace GParse.Lexing.Composable
         /// <param name="start"></param>
         /// <param name="end"></param>
         /// <returns></returns>
-        public static CharacterRange Range ( Char start, Char end ) =>
-            new CharacterRange ( start, end );
+        public static CharacterRange Range ( Char start, Char end ) => new ( start, end );
 
         /// <summary>
         /// Creates a new <see cref="Composable.Set"/>.
         /// </summary>
         /// <param name="setElements"></param>
         /// <returns></returns>
-        public static Set Set ( params SetElement[] setElements ) =>
-            new Set ( setElements );
+        public static Set Set ( params SetElement[] setElements ) => new ( setElements );
 
         /// <summary>
         /// Creates a new <see cref="NumberedBackreference"/>.
         /// </summary>
         /// <param name="position"></param>
         /// <returns></returns>
-        public static NumberedBackreference Backreference ( Int32 position ) =>
-            new NumberedBackreference ( position );
+        public static NumberedBackreference Backreference ( Int32 position ) => new ( position );
 
         /// <summary>
         /// Creates a new <see cref="NamedBackreference"/>.
         /// </summary>
         /// <param name="name"></param>
         /// <returns></returns>
-        public static NamedBackreference Backreference ( String name ) =>
-            new NamedBackreference ( name );
+        public static NamedBackreference Backreference ( String name ) => new ( name );
 
         /// <summary>
         /// Creates a new <see cref="NumberedCapture"/>.
@@ -79,8 +71,7 @@ namespace GParse.Lexing.Composable
         /// <param name="position"></param>
         /// <param name="innerNode"></param>
         /// <returns></returns>
-        public static NumberedCapture Capture ( Int32 position, GrammarNode<Char> innerNode ) =>
-            new NumberedCapture ( position, innerNode );
+        public static NumberedCapture Capture ( Int32 position, GrammarNode<Char> innerNode ) => new ( position, innerNode );
 
         /// <summary>
         /// Creates a new <see cref="NamedCapture"/>.
@@ -88,32 +79,28 @@ namespace GParse.Lexing.Composable
         /// <param name="name"></param>
         /// <param name="innerNode"></param>
         /// <returns></returns>
-        public static NamedCapture Capture ( String name, GrammarNode<Char> innerNode ) =>
-            new NamedCapture ( name, innerNode );
+        public static NamedCapture Capture ( String name, GrammarNode<Char> innerNode ) => new ( name, innerNode );
 
         /// <summary>
         /// Creates a new <see cref="Composable.Lookahead"/>.
         /// </summary>
         /// <param name="innerNode"></param>
         /// <returns></returns>
-        public static Lookahead Lookahead ( GrammarNode<Char> innerNode ) =>
-            new Lookahead ( innerNode );
+        public static Lookahead Lookahead ( GrammarNode<Char> innerNode ) => new ( innerNode );
 
         /// <summary>
         /// Creates a new <see cref="Sequence{T}"/>.
         /// </summary>
         /// <param name="grammarNodes"></param>
         /// <returns></returns>
-        public static Sequence<Char> Sequence ( params GrammarNode<Char>[] grammarNodes ) =>
-            new Sequence<Char> ( grammarNodes );
+        public static Sequence<Char> Sequence ( params GrammarNode<Char>[] grammarNodes ) => new ( grammarNodes );
 
         /// <summary>
         /// Creates a new <see cref="Alternation{T}"/>.
         /// </summary>
         /// <param name="grammarNodes"></param>
         /// <returns></returns>
-        public static Alternation<Char> Alternation ( params GrammarNode<Char>[] grammarNodes ) =>
-            new Alternation<Char> ( grammarNodes );
+        public static Alternation<Char> Alternation ( params GrammarNode<Char>[] grammarNodes ) => new ( grammarNodes );
 
         /// <summary>
         /// Creates a new <see cref="Repetition{T}"/>.
@@ -122,6 +109,6 @@ namespace GParse.Lexing.Composable
         /// <param name="repetitionRange"></param>
         /// <returns></returns>
         public static Repetition<Char> Repetition ( GrammarNode<Char> node, RepetitionRange repetitionRange ) =>
-            new Repetition<Char> ( node, repetitionRange, false );
+            new ( node, repetitionRange, false );
     }
 }
